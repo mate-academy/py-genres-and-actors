2c901cdee71e6a0405c84837db0e6e0f2d363c38
Rybak Dmytro <codingclaud@gmail.com>
2022-02-23 13:32:22 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,34 @@
 import init_django_orm  # noqa: F401
+from db.models import Genre, Actors
 
 
 def main():
-    pass
+    # Create Genres
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+    # Create Actors
+    Actors.objects.create(first_name="George", last_name="Klooney", )
+    Actors.objects.create(first_name="Kianu", last_name="Reaves", )
+    Actors.objects.create(first_name="Scarlett", last_name="Keegan", )
+    Actors.objects.create(first_name="Will", last_name="Smith", )
+    Actors.objects.create(first_name="Jaden", last_name="Smith")
+    Actors.objects.create(first_name="Scarlett", last_name="Johansson")
+    # Update
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actors.objects.filter(first_name="George", last_name="Klooney").update(
+        last_name="Clooney"
+    )
+    Actors.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
+    # Delete
+    Genre.objects.filter(name="Action").delete()
+    Actors.objects.filter(first_name="Scarlett").delete()
+
+    return Actors.objects.filter(last_name="Smith").order_by("first_name")
+
+
+if __name__ == "__main__":
+    print(main())
