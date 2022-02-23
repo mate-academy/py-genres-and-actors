90b2bc328fdaf81073a338e885e67cee8e21222f
Artur Shavula <arthuromgnh@gmail.com>
2022-02-23 13:41:26 +0200

    Solution

--- app/db/models.py
+++ app/db/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    _name_max_length = 255
+    name = models.CharField(
+        max_length=_name_max_length
+    )
+
+
+class Actor(models.Model):
+    _first_name_max_length = 255
+    _last_name_max_length = 255
+    first_name = models.CharField(
+        max_length=_first_name_max_length
+    )
+    second_name = models.CharField(
+        max_length=_last_name_max_length
+    )
--- app/main.py
+++ app/main.py
@@ -1,5 +1,56 @@
 import init_django_orm  # noqa: F401
 
 
+from db.models import Genre, Actor
+
+
 def main():
-    pass
+    genres_to_create = [
+        'Western',
+        'Action',
+        'Dramma',
+    ]
+    actors_to_create = [
+        'Kianu Reaves',
+        'Scarlett Keegan',
+        'Will Smith',
+        'Jaden Smith',
+        'Scarlett Johansson',
+    ]
+    for genre in genres_to_create:
+        Genre.objects.create(
+            name=genre
+        )
+
+    for actor in actors_to_create:
+        first_name, last_name = actor.split()
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+    Genre.objects.filter(
+        name='Dramma'
+    ).update(name='Drama')
+
+    Actor.objects.filter(
+        first_name='George',
+        last_name='Klooney',
+    ).update(last_name='Clooney')
+
+    Actor.objects.filter(
+        first_name='Kianu',
+        last_name='Reaves',
+    ).update(last_name='Reeves')
+
+    Genre.objects.filter(
+        name='Actor'
+    ).delete()
+
+    Actor.object.filter(
+        first_name='Scarlett'
+    ).delete()
+
+    return Actor.objects.filter(
+        last_name='Smith'
+    ).order_by('first_name')
