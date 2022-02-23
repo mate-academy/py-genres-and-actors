f4833ac29ffc6e750774590d020b484f65181579
Nazar-kob <kobrinnazar05@mail.com>
2022-02-23 13:42:49 +0200

    new Solution

--- app/db/models.py
+++ app/db/models.py
@@ -6,7 +6,7 @@ class Genre(models.Model):
     name = models.CharField(max_length=255)
 
 
-class Actors(models.Model):
+class Actor(models.Model):
 
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import init_django_orm  # noqa: F401
 
-from db.models import Genre, Actors
+from db.models import Genre, Actor
 
 
 def main():
@@ -42,28 +42,24 @@ def main():
     ]
 
     for actor in list_of_actors:
-        Actors.objects.create(**actor)
+        Actor.objects.create(**actor)
 
     # UPDATE
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actors.objects.filter(first_name="George",
-                          last_name="Klooney"
-                          ).update(
-        last_name="Clooney")
-    Actors.objects.filter(first_name="Kianu",
-                          last_name="Reaves"
-                          ).update(first_name="Keanu",
-                                   last_name="Reeves")
+    Actor.objects.filter(first_name="George", last_name="Klooney")\
+        .update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves")\
+        .update(first_name="Keanu", last_name="Reeves")
 
     # DELETE
 
     Genre.objects.filter(name="Action").delete()
-    Actors.objects.filter(first_name="Scarlett").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     # READ
 
-    return Actors.objects.filter(last_name="Smith").order_by("first_name")
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
 if __name__ == "__main__":
