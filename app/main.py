f02ca9c5b6e908abae1424dbf7c9ed7b80aa264a
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-23 15:10:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -9,10 +9,11 @@ def main():
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Reaves")
-    Actor.objects.create(first_name="Kianu", last_name="Klooney")
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
     Actor.objects.create(first_name="Scarlett", last_name="Keegan")
     Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     # updating
