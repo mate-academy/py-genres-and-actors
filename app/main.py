590639dfd3d5e515c0114804b8189e4b467e36aa
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-23 13:33:42 +0200

    rename Actors -> Actor

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import init_django_orm  # noqa: F401
 
-from db.models import Genre, Actors
+from db.models import Genre, Actor
 
 
 def main():
@@ -9,20 +9,20 @@ def main():
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
 
-    Actors.objects.create(first_name="George", last_name="Klooney")
-    Actors.objects.create(first_name="Kianu", last_name="Reaves")
-    Actors.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actors.objects.create(first_name="Will", last_name="Smith")
-    Actors.objects.create(first_name="Jaden", last_name="Smith")
-    Actors.objects.create(first_name="Scarlett", last_name="Johansson")
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
-    Actors.objects.filter(
+    Actor.objects.filter(
         first_name="George", last_name="Klooney"
     ).update(last_name="Clooney")
-    Actors.objects.filter(
+    Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
 
@@ -30,9 +30,9 @@ def main():
 
     Genre.objects.filter(name="Action").delete()
 
-    Actors.objects.filter(first_name="Scarlett").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actors.objects.filter(last_name="Smith").order_by("first_name")
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
 if __name__ == '__main__':
