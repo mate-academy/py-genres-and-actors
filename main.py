f30a0f999ff0e7206114fc4403df77b29777a30b
Roma Tsybulia <Romalonstejn@gmail.com>
2022-06-16 18:28:39 +0300

    Refactored funtion 'main' in 'main.py'

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from db.models import Genre, Actor
 
 
 def main():
+
     # create genres and actors
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    genres = ["Western", "Action", "Dramma"]
+    actors = [
+        ["George", "Klooney"],
+        ["Kianu", "Reaves"],
+        ["Scarlett", "Keegan"],
+        ["Will", "Smith"],
+        ["Jaden", "Smith"],
+        ["Scarlett", "Johansson"],
+    ]
+
+    for genre in genres:
+        Genre.objects.create(name=genre)
+
+    for actor in actors:
+        Actor.objects.create(first_name=actor[0], last_name=actor[1])
 
     # update genres and actors
     Genre.objects.filter(name="Dramma").update(name="Drama")
