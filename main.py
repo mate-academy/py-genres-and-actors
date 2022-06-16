50a2de8c26bd74402af7d7bc874cfb880ec1b5f7
Dima Svirsa <august4smile@gmail.com>
2022-06-16 14:33:39 +0300

    better data

--- main.py
+++ main.py
@@ -4,25 +4,29 @@ from db.models import Genre, Actor
 
 
 def main():
+    genres = [{"name": "Western"},
+              {"name": "Action"},
+              {"name": "Dramma"}]
+    for genre in genres:
+        Genre.objects.create(**genre)
 
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    actors = [{"first_name": "George", "last_name": "Klooney"},
+              {"first_name": "Kianu", "last_name": "Reaves"},
+              {"first_name": "Scarlett", "last_name": "Keegan"},
+              {"first_name": "Will", "last_name": "Smith"},
+              {"first_name": "Jaden", "last_name": "Smith"},
+              {"first_name": "Scarlett", "last_name": "Johansson"}]
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    for actor in actors:
+        Actor.objects.create(**actor)
 
-    Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Genre.objects.filter(
+        name="Dramma").update(name="Drama")
     Actor.objects.filter(
-        first_name="Kianu", last_name="Reaves"
-    ).update(
-        first_name="Keanu", last_name="Reeves"
-    )
+        last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
