3bda2486a047a524a72839ae128384bc9ed635d8
Yurnerroo <yura.zdanevych@gmail.com>
2022-06-17 17:45:07 +0300

    'Solution'

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import init_django_orm  # noqa: F401
 
+from db.models import Actor, Genre
+
 
 def main():
-    pass
+    # create
+    genres_create = ["Western", "Action", "Dramma"]
+
+    for genre in genres_create:
+        Genre.objects.create(name=genre)
+
+    actors_create = [
+        ["George", "Klooney"],
+        ["Kianu", "Reaves"],
+        ["Scarlett", "Keegan"],
+        ["Will", "Smith"],
+        ["Jaden", "Smith"],
+        ["Scarlett", "Johansson"]
+    ]
+
+    for actor in actors_create:
+        Actor.objects.create(first_name=actor[0], last_name=actor[1])
+
+    # update
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu").update(
+        first_name="Keanu", last_name="Reeves"
+    )
+    # delete
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
