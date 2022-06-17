14e5962d82caac70234416e8488a4fd31b43512d
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-06-17 21:35:48 +0300

    'Fixed_filter_and_one_of_the_tasks'

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from db.models import Actor, Genre
 
 def main():
     genres = ["Western", "Action", "Dramma"]
-    actors = ["George Klooney", "Keanu Reaves",
+    actors = ["George Klooney", "Kianu Reaves",
               "Scarlett Keegan", "Will Smith",
               "Jaden Smith", "Scarlett Johansson"]
     for genre in genres:
@@ -16,8 +16,8 @@ def main():
         last_name = separated_actor[1]
         Actor.objects.create(first_name=name, last_name=last_name)
 
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(last_name="Reaves").update(last_name="Reeves")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
     Actor.objects.filter(first_name="Scarlett").delete()
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
