28d2e496488c02a9b422c9403beb15b29f37e88f
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2022-06-17 16:00:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,17 @@ from db.models import Actor
 
 
 def main():
-    Genre.objects.create("Western")
-    Genre.objects.create("Action")
-    Genre.objects.create("Dramma")
-    Genre.objects.create("Dramma")
-    Genre.objects.create("Dramma")
-    Actor.objects.create("George", "Klooney")
-    Actor.objects.create("Kianu", "Reaves")
-    Actor.objects.create("Scarlett", "Keegan")
-    Actor.objects.create("Will", "Smith")
-    Actor.objects.create("Jaden", "Smith")
-    Actor.objects.create("Scarlett", "Johansson")
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
-    Genre.objects.filter("Dramma").update("Drama")
+    Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
         first_name="George", last_name="Klooney"
     ).update(last_name="Clooney")
@@ -25,7 +23,7 @@ def main():
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
 
-    Genre.objects.filter("Action").delete()
+    Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(last_name="Smith").all().order_by("first_name")
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
