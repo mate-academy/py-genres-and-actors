da8de52de70d0dece0258fc463d6e25498e85948
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2022-06-17 15:33:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import init_django_orm  # noqa: F401
 from db.models import Genre
 from db.models import Actor
 
+
 def main():
-#Create
     Genre.objects.create("Western")
     Genre.objects.create("Action")
     Genre.objects.create("Dramma")
@@ -16,12 +16,16 @@ def main():
     Actor.objects.create("Will", "Smith")
     Actor.objects.create("Jaden", "Smith")
     Actor.objects.create("Scarlett", "Johansson")
-#Update
+
     Genre.objects.filter("Dramma").update("Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
-#Delete
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+
     Genre.objects.filter("Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
-#Read
+
     return Actor.objects.filter(last_name="Smith").all().order_by("first_name")
