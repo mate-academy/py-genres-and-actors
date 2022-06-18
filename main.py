33560770a496e0e08d613ef2149407cf56f38c73
Dorius <kornelydorius@gmail.com>
2022-06-18 17:53:30 +0300

    'Solution'

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import init_django_orm  # noqa: F401
 from db.models import Genre, Actor
 
-# Delete:
-# genre Action
-# all actresses with the first_name "Scarlett"
 
-
+# Shitcode alert!
 def main():
+    # Create
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
@@ -17,10 +15,16 @@ def main():
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
+    # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
 
+    # Delete
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
