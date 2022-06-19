f57245425a10aa14060d8f1c2da9310c76340822
Artem Toporchan <gydratvar@gmail.com>
2022-06-19 13:11:55 +0300

    Ok

--- main.py
+++ main.py
@@ -4,40 +4,32 @@ from db.models import Actor, Genre
 
 
 def main():
-    Genre.objects.create(
-        name="Western"
-    )
-    Genre.objects.create(
-        name="Action"
-    )
-    Genre.objects.create(
-        name="Dramma"
-    )
+    genres = ["Western", "Action", "Dramma"]
+    actors = [
+        {"first_name": "George",
+         "last_name": "Klooney", },
+        {"first_name": "Kianu",
+         "last_name": "Reaves", },
+        {"first_name": "Scarlett",
+         "last_name": "Keegan", },
+        {"first_name": "Will",
+         "last_name": "Smith", },
+        {"first_name": "Jaden",
+         "last_name": "Smith", },
+        {"first_name": "Scarlett",
+         "last_name": "Johansson", },
+    ]
 
-    Actor.objects.create(
-        first_name="George",
-        last_name="Klooney"
-    )
-    Actor.objects.create(
-        first_name="Kianu",
-        last_name="Reaves"
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Keegan"
-    )
-    Actor.objects.create(
-        first_name="Will",
-        last_name="Smith"
-    )
-    Actor.objects.create(
-        first_name="Jaden",
-        last_name="Smith"
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Johansson"
-    )
+    for genre in genres:
+        Genre.objects.create(
+            name=genre
+        )
+
+    for actor in actors:
+        Actor.objects.create(
+            first_name=actor["first_name"],
+            last_name=actor["last_name"]
+        )
 
     Genre.objects.filter(
         name="Dramma"
