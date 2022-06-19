bd8a3eee27b416123ffe3500b6aac2b5f15759c9
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-06-19 16:42:12 +0300

    Solution

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from db.models import Genre, Actor
 
 genres = ["Western", "Action", "Dramma"]
 actors = [
-            {"first_name": "George", "last_name": "Klooney"},
-            {"first_name": "Kianu", "last_name": "Reaves"},
-            {"first_name": "Scarlett", "last_name": "Keegan"},
-            {"first_name": "Will", "last_name": "Smith"},
-            {"first_name": "Jaden", "last_name": "Smith"},
-            {"first_name": "Scarlett", "last_name": "Johansson"}
-          ]
+    {"first_name": "George", "last_name": "Klooney"},
+    {"first_name": "Kianu", "last_name": "Reaves"},
+    {"first_name": "Scarlett", "last_name": "Keegan"},
+    {"first_name": "Will", "last_name": "Smith"},
+    {"first_name": "Jaden", "last_name": "Smith"},
+    {"first_name": "Scarlett", "last_name": "Johansson"}
+]
 
 
 def main():
@@ -30,7 +30,3 @@ def main():
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == '__main__':
-    main()
