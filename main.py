9348d7a5e85bc50aaece119c42b22a04ab464d21
Vasyl Smutok <23.smutok@gmail.com>
2022-06-27 12:01:22 +0300

    Second Solution

--- main.py
+++ main.py
@@ -25,7 +25,3 @@ def main():
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == '__main__':
-    main()
