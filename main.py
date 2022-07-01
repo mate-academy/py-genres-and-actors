16a1fd0575eb15c0627d00e71646b649d619e8f4
Anton Lasko <anton.lasko1993@gmail.com>
2022-07-01 16:46:56 +0300

    Solution

--- main.py
+++ main.py
@@ -7,51 +7,51 @@ def main():
 
     # CREATE
 
-    new_genre1 = Genre.objects.create(
+    Genre.objects.create(
         name="Western",
     )
-    new_genre2 = Genre.objects.create(
+    Genre.objects.create(
         name="Action",
     )
-    new_genre3 = Genre.objects.create(
+    Genre.objects.create(
         name="Dramma",
     )
 
-    new_actor1 = Actor.objects.create(
+    Actor.objects.create(
         first_name="George",
         last_name="Klooney",
     )
-    new_actor2 = Actor.objects.create(
+    Actor.objects.create(
         first_name="Kianu",
         last_name="Reaves",
     )
-    new_actor3 = Actor.objects.create(
+    Actor.objects.create(
         first_name="Scarlett",
         last_name="Keegan",
     )
-    new_actor4 = Actor.objects.create(
+    Actor.objects.create(
         first_name="Will",
         last_name="Smith",
     )
-    new_actor5 = Actor.objects.create(
+    Actor.objects.create(
         first_name="Jaden",
         last_name="Smith",
     )
-    new_actor6 = Actor.objects.create(
+    Actor.objects.create(
         first_name="Scarlett",
         last_name="Johansson",
     )
 
     # UPDATE
 
-    filtered_genre1 = Genre.objects.filter(
+    Genre.objects.filter(
         name="Dramma",
     ).update(name="Drama")
 
-    filtered_actor1 = Actor.objects.filter(
+    Actor.objects.filter(
         last_name="Klooney",
     ).update(last_name="Clooney")
-    filtered_actor2 = Actor.objects.filter(
+    Actor.objects.filter(
         first_name="Kianu",
         last_name="Reaves",
     ).update(first_name="Keanu", last_name="Reeves")
