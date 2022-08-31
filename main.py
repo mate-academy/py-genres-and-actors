62c729ba256011bb96769e5bc5948d736b3cb14c
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-08-31 19:42:09 +0300

    Solution

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
     Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reeves")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
     Actor.objects.create(first_name="Scarlett", last_name="Keegan")
     Actor.objects.create(first_name="Will", last_name="Smith")
     Actor.objects.create(first_name="Jaden", last_name="Smith")
@@ -16,7 +16,9 @@ def main():
     # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu")
+    Actor.objects.filter(
+        first_name="Kianu"
+    ).update(first_name="Keanu", last_name="Reeves")
     # Delete
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
