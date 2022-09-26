d76567cc317270cdb794598683596dee5a1bd5f1
Volodymyr Panov <vpanov25@gmail.com>
2022-09-26 18:23:50 +0200

    Solution

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def main():
 
     Genre.objects.filter(name="Dramma",).update(name="Drama")
     Actor.objects.filter(last_name="Klooney",).update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves",).update(first_name="Keanu", last_name="Reeves",)
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves",)\
+        .update(first_name="Keanu", last_name="Reeves",)
 
     Actor.objects.filter(first_name="Scarlett",).delete()
     Genre.objects.filter(name="Action",).delete()
