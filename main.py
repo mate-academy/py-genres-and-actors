10edd41fad4ea21196a8745e2afbb4e050ec0d87
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-10-09 16:06:03 +0300

    Run flake8

--- main.py
+++ main.py
@@ -24,20 +24,27 @@ def main() -> QuerySet:
                          last_name="Johansson")
 
     # Update
-    Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(
+    Genre.objects.filter(name="Dramma").update(
+        name="Drama"
+    )
+    Actor.objects.filter(first_name="George",
+                         last_name="Klooney").update(
         last_name="Clooney"
     )
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+    Actor.objects.filter(first_name="Kianu",
+                         last_name="Reaves").update(
         first_name="Keanu", last_name="Reeves"
     )
 
     # Delete
     Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(first_name="Scarlett").delete()
+    Actor.objects.filter(
+        first_name="Scarlett").delete()
 
     # Return
-    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+    return Actor.objects.filter(
+        last_name="Smith"
+    ).order_by("first_name")
 
 
 if __name__ == '__main__':
