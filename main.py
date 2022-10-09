5d82140cb4df78473cd07ff00c51423b799032f6
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-10-09 23:30:49 +0300

    Changed create method using loops

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ from db.models import Genre, Actor
 
 def main() -> QuerySet:
     # Create
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George",
-                         last_name="Klooney")
-    Actor.objects.create(first_name="Kianu",
-                         last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett",
-                         last_name="Keegan")
-    Actor.objects.create(first_name="Will",
-                         last_name="Smith")
-    Actor.objects.create(first_name="Jaden",
-                         last_name="Smith")
-    Actor.objects.create(first_name="Scarlett",
-                         last_name="Johansson")
+    genres_list = [
+        "Western", "Action", "Dramma"
+    ]
+
+    actors_list = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+
+    for genre in genres_list:
+        Genre.objects.create(name=genre)
+
+    for actor in actors_list:
+        Actor.objects.create(first_name=actor[0],
+                             last_name=actor[1])
 
     # Update
     Genre.objects.filter(name="Dramma").update(
