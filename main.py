950f23af4ebe9188f855b446168bac16a4f27277
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-10-16 15:14:43 +0300

    Solution

--- main.py
+++ main.py
@@ -7,16 +7,25 @@ from db.models import Genre, Actor
 
 def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
+
     for genre in genres:
         Genre.objects.create(name=genre)
 
-    actors = ["George Klooney", "Kianu Reaves", "Scarlett Keegan", "Will Smith", "Jaden Smith", "Scarlett Johansson"]
+    actors = ["George Klooney",
+              "Kianu Reaves",
+              "Scarlett Keegan",
+              "Will Smith",
+              "Jaden Smith",
+              "Scarlett Johansson"]
+
     for actor in actors:
-        Actor.objects.create(first_name=actor.split()[0], last_name=actor.split()[1])
+        Actor.objects.create(first_name=actor.split()[0],
+                             last_name=actor.split()[1])
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
@@ -24,5 +33,5 @@ def main() -> QuerySet:
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
