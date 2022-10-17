658240dd0a76070a8b2f4eeb454be7d7caa8bcd9
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-17 17:26:22 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
-from db.models import Genre, Actor, Actress
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
