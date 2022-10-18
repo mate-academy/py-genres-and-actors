78c0530f7150b1a3a850ce65ff805cffdda89152
Yura <pazyn404@gamil.com>
2022-10-18 12:58:29 +0300

    Fixed creation

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ from db.models import Genre, Actor
 
 def main() -> QuerySet:
     # create
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    genre_data = [
+        "Western",
+        "Action",
+        "Dramma"
+    ]
+    for name in genre_data:
+        Genre.objects.create(name=name)
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    actors_data = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+    for first_name, last_name in actors_data:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
     # update
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
