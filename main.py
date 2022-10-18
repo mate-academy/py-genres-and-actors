4c5b7b46b80e2507fc41445d6658f0dea61f0044
Zakhar Podporinov <zp162162@gmail.com>
2022-10-18 18:29:11 +0300

    Resolve remark

--- main.py
+++ main.py
@@ -41,7 +41,3 @@ def main() -> QuerySet:
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.all().filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == "__main__":
-    print(main())
