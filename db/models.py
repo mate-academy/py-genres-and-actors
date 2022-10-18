e54d28b876e3d3d126f4579ae81292ce054b054d
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-10-18 18:17:26 +0300

    Fixed max_length in Genre and Actor classes

--- db/models.py
+++ db/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=255)
 
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=35)
-    last_name = models.CharField(max_length=35)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
