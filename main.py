e9f400ac6eae4ab5d21a7f3d4ccac7d01d308fe9
Katrin Lazarenko <katrin903@gmail.com>
2022-10-19 09:37:41 +0300

    Deleted nested functions

--- main.py
+++ main.py
@@ -6,41 +6,40 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    def fill_in_tables() -> None:
-        genres = ["Western", "Action", "Dramma"]
-
-        for genre in genres:
-            Genre.objects.create(name=genre)
-
-        actors = [
-            ("George", "Klooney"),
-            ("Kianu", "Reaves"),
-            ("Scarlett", "Keegan"),
-            ("Will", "Smith"),
-            ("Jaden", "Smith"),
-            ("Scarlett", "Johansson")
-        ]
-
-        for name, surname in actors:
-            Actor.objects.create(first_name=name, last_name=surname)
-
     fill_in_tables()
-
-    def update_tables() -> None:
-        Genre.objects.filter(name="Dramma").update(name="Drama")
-        Actor.objects.filter(first_name="George",
-                             last_name="Klooney").update(last_name="Clooney")
-        Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
-            first_name="Keanu", last_name="Reeves"
-        )
-
     update_tables()
-
-    def delete_from_tables() -> None:
-        Genre.objects.filter(name="Action").delete()
-        Actor.objects.filter(first_name="Scarlett").delete()
-
     delete_from_tables()
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
-    result = Actor.objects.filter(last_name="Smith").order_by("first_name")
-    return result
+
+def fill_in_tables() -> None:
+    genres = ["Western", "Action", "Dramma"]
+
+    for genre in genres:
+        Genre.objects.create(name=genre)
+
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+
+    for name, surname in actors:
+        Actor.objects.create(first_name=name, last_name=surname)
+
+
+def update_tables() -> None:
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George",
+                         last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves"
+    )
+
+
+def delete_from_tables() -> None:
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
