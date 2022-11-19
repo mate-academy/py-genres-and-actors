ca9eb6304683576d944432c59e9438570bf8fc98
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-11-19 13:58:48 +0200

    Solution1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import init_django_orm  # noqa: F401
 from django.db.models import QuerySet
 from db.models import Actor, Genre
 
+
 def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
     actors = [
@@ -30,9 +31,7 @@ def main() -> QuerySet:
         first_name="Kianu",
         last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
 
-
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(last_name="Smith").order_by('first_name')
-
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
