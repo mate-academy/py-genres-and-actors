1d1b8cacd61e93c68be2e20a357dc2f2e734b0c8
Anton Kamenetskyi <kamenetskyy@gmail.com>
2022-11-30 14:38:30 +0100

    Solution after requested changes

--- main.py
+++ main.py
@@ -11,21 +11,35 @@ def main() -> QuerySet:
     for genre in genres_to_create:
         Genre.objects.create(name=genre)
 
-    actors_to_create = [("George", "Klooney"), ("Kianu", "Reaves"),
-                        ("Scarlett", "Keegan"), ("Will", "Smith"),
-                        ("Jaden", "Smith"), ("Scarlett", "Johansson")]
+    actors_to_create = [
+        ("George", "Klooney"), ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"), ("Will", "Smith"),
+        ("Jaden", "Smith"), ("Scarlett", "Johansson")
+    ]
 
     for first_name, last_name in actors_to_create:
-        Actor.objects.create(first_name=first_name, last_name=last_name)
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name
+        )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
-    Actor.objects.filter(first_name="George", last_name="Klooney") \
-        .update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves") \
-        .update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney").update(
+        last_name="Clooney"
+    )
+
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
+
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
