239cd277007830852a691a281323c2ff36ab60da
Daria Hannochenko <soloha7965@gmail.com>
2022-12-27 18:54:26 +0200

    Solution

--- main.py
+++ main.py
@@ -10,22 +10,18 @@ def main() -> QuerySet:
     Genre.objects.create(name="Dramma")
     Actor.objects.create(first_name="George", last_name="Klooney")
     Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett ", last_name="Keegan")
-    Actor.objects.create(first_name="Will ", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves")
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
-    # del_scarlett = Actor.objects.filter(first_name="Scarlett").delete()
-    # if not del_scarlett:
-    #     print("The Queryset is empty ")
-    # else:
-    #     print("The Queryset has results ...")
     return Actor.objects.filter(last_name="Smith").order_by("first_name").all()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(main())
