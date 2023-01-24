c87689df2839173f95479f100b223d969931135a
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-01-24 17:26:55 +0200

    Solution

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def main() -> QuerySet:
         first_name="Keanu", last_name="Reeves"
     )
 
-    Genre.objects.filter(name="Action").delete()
+    Genre.objects.filter(name="Action",).delete()
 
-    Actor.objects.filter(first_name="Scarlett").delete()
+    Actor.objects.filter(first_name="Scarlett",).delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
