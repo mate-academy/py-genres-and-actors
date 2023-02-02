a071864dcc3a0cb5c3505ae4785eb558474f288b
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-02-02 15:18:01 +0200

    'upd_loops_Solution'

--- main.py
+++ main.py
@@ -5,15 +5,24 @@ from db.models import Actor, Genre
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    genre_list = ["Western", "Action", "Dramma"]
+    actors_list = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+
+    for genre in genre_list:
+        Genre.objects.create(name=genre)
+
+    for actor_name, actor_last_name in actors_list:
+        Actor.objects.create(
+            first_name=actor_name,
+            last_name=actor_last_name
+        )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George", last_name="Klooney").update(
