723f0b757504b27567d9306a4780e0829203bfd4
Dmytro <omelchenko.dm@gmail.com>
2023-02-03 17:44:32 +0200

    Genres_Actors

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ def main() -> QuerySet:
     actors = [("George", "Klooney"), ("Kianu", "Reaves"),
               ("Scarlett", "Keegan"), ("Will", "Smith"),
               ("Jaden", "Smith"), ("Scarlett", "Johansson")]
-    # Create genres
+
     for genre in genres:
         Genre.objects.create(name=genre)
-    # Create Actors
+
     for name, surname in actors:
         Actor.objects.create(
             first_name=name,
             last_name=surname
         )
-    # Update genres
+
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    # Update actors
+
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(
         first_name="Kianu",
@@ -29,10 +29,10 @@ def main() -> QuerySet:
     ).update(
         first_name="Keanu", last_name="Reeves"
     )
-    # Delete genres
+
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
-    # Read actors
+
     filtered_actors = Actor.objects.filter(
         last_name="Smith"
     ).order_by("first_name")
