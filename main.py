75e5ee931824e4bd6c982d592138ad5f276393d4
Oleksandr Mamchur <irex1988@gmail.com>
2023-02-06 21:45:07 +0200

    'fixed_commas'

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
     # Update
-    Genre.objects.filter(name="Dramma", ).update(name="Drama")
-    Actor.objects.filter(last_name="Klooney", ).update(last_name="Clooney")
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
     # Delete
-    Genre.objects.filter(name="Action", ).delete()
-    Actor.objects.filter(first_name="Scarlett", ).delete()
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
     # Return
-    return Actor.objects.filter(last_name="Smith", ).order_by("first_name")
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
 if __name__ == "__main__":
