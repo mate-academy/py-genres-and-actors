b97081e925fd217aadab550f1118b45094d81286
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-02-19 11:51:46 +0200

    update tables

--- db/models.py
+++ db/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=63)
+    name = models.CharField(max_length=255)
 
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
