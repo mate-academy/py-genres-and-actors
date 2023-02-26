759379202c1575a724b151088f5489cc18d8fa59
Viacheslav Shcherbak <misternictof@gmail.com>
2023-02-26 12:15:45 +0200

    AUse cycleÑ

--- main.py
+++ main.py
@@ -5,33 +5,24 @@ from django.db.models import QuerySet
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(
-        first_name="George",
-        last_name="Klooney"
-    )
-    Actor.objects.create(
-        first_name="Kianu",
-        last_name="Reaves"
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Keegan"
-    )
-    Actor.objects.create(
-        first_name="Will",
-        last_name="Smith"
-    )
-    Actor.objects.create(
-        first_name="Jaden",
-        last_name="Smith"
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Johansson"
-    )
+    genre_to_create = ["Western", "Action", "Dramma"]
+    for genre in genre_to_create:
+        Genre.objects.create(name=genre)
+
+    actors_to_create = [
+        {"first_name": "George", "last_name": "Klooney"},
+        {"first_name": "Kianu", "last_name": "Reaves"},
+        {"first_name": "Scarlett", "last_name": "Keegan"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Jaden", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
+    ]
+    for actor in actors_to_create:
+        Actor.objects.create(
+            first_name=actor["first_name"],
+            last_name=actor["last_name"]
+        )
+
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
         last_name="Klooney"
@@ -41,8 +32,6 @@ def main() -> QuerySet:
         last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
     Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(
-        first_name="Scarlett"
-    ).delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
