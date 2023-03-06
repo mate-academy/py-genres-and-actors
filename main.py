d143bd927f5ffab933438de333bcec334f94aab2
Vadym Bihun <bihunva@gmail.com>
2023-03-06 16:06:01 +0200

    Added a more informative filter

--- main.py
+++ main.py
@@ -11,25 +11,28 @@ def main() -> QuerySet:
         "Action",
         "Dramma"
     ]
+
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+
     for genre in genres:
         Genre.objects.create(name=genre)
 
-    actors = [
-        "George Klooney",
-        "Kianu Reaves",
-        "Scarlett Keegan",
-        "Will Smith",
-        "Jaden Smith",
-        "Scarlett Johansson"
-    ]
-    for actor in actors:
-        Actor.objects.create(
-            first_name=actor.split()[0],
-            last_name=actor.split()[1]
-        )
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney"
+    ).update(last_name="Clooney")
+
     Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
