47a614d4add438e5600aa124f4a4fb116d291d72
Max Bilokur <bilmaxim@gmail.com>
2023-03-07 16:25:23 +0200

    update fixed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from db.models import Actor, Genre
 
 def main() -> QuerySet:
     genres_to_create = ["Western", "Action", "Dramma"]
-    actors_to_create = [("George", "Klooney"), ("Kianu", "Reeves"),
+    actors_to_create = [("George", "Klooney"), ("Kianu", "Reaves"),
                         ("Scarlett", "Keegan"), ("Will", "Smith"),
                         ("Jaden", "Smith"), ("Scarlett", "Johansson")]
 
@@ -19,7 +19,10 @@ def main() -> QuerySet:
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
