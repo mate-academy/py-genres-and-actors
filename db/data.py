f892b60fbf43cd44924b25f9b56ffc9a2234a92a
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-03-07 17:32:31 +0200

    Moved 'data' to separate file

---
+++ db/data.py
@@ -0,0 +1,15 @@
+DATA = [
+    (
+        "Western",
+        "Action",
+        "Dramma"
+    ),
+    (
+        "George Klooney",
+        "Kianu Reaves",
+        "Scarlett Keegan",
+        "Will Smith",
+        "Jaden Smith",
+        "Scarlett Johansson"
+    )
+]
