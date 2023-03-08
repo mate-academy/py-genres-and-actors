372f63ae829d16650d3e5737fe14bd175e1b7f68
Paul Savchuk <nekrjkeee@gmail.com>
2023-03-08 21:20:25 +0200

    removed redundant test model

--- db/models.py
+++ db/models.py
@@ -8,8 +8,3 @@ class Genre(models.Model):
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-
-
-class Concert(models.Model):
-    name = models.CharField(max_length=63)
-    augience = models.Fi
