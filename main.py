d8b1d7fd08354ee68d0ae6e8be66952f5768ff80
Artur Tytomyr <art.tytomyr@gmail.com>
2023-03-10 10:26:28 +0200

    revised

--- main.py
+++ main.py
@@ -50,4 +50,8 @@ def main() -> QuerySet:
 
     return Actor.objects.filter(
         last_name="Smith"
-    ).order_by("first_name").values()
+    ).order_by("first_name")
+
+
+if __name__ == "__main__":
+    main()
