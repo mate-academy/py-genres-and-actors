9c356cb1d801d48e1dc9a45d11f6dd1ac8700d1d
Sashkoooo <o.denysenko@hotmail.com>
2023-04-06 22:41:21 +0300

    Solution V1.0

--- db/models.py
+++ db/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    """Create table genre"""
+    name = models.CharField(max_length=255)
+
+
+class Actor(models.Model):
+    """Create table actor"""
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
