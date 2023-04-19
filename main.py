dec89aa65f4540cce2d55d2ee8df966fb97ce625
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-04-19 13:09:09 +0300

    4

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main() -> QuerySet:
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
-    ).update(first_name="Keanu").update(last_name="Reeves")
+    ).update(first_name="Keanu", last_name="Reeves")
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
     return Actor.objects.filter(first_name="Smith").order_by("first_name")
