aa24dc5b61f9b26587ab2e781aad4e9bef829038
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-04-21 00:12:58 +0300

    Solution

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 def main() -> QuerySet:
-    #############Creating genres#############
+    #Creating genres
     genres = ["Western", "Action", "Dramma"]
 
     for genre in genres:
         Genre.objects.create(name=genre)
-    #############Creating actors#############
+    #Creating actors
     actors = [("George", "Klooney"),
               ("Kianu", "Reaves"),
               ("Scarlett", "Keegan"),
@@ -21,18 +21,18 @@ def main() -> QuerySet:
     for first_name, last_name in actors:
         Actor.objects.create(first_name=first_name,
                              last_name=last_name)
-    ###################Updating genres######################
+    #Updating genres
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    ##################Updating last_name#######################
+    #Updating last_name
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    ##################Updating actor##########################
+    #Updating actor
     Actor.objects.filter(last_name="Reaves").update(
         first_name="Keanu",
         last_name="Reeves"
     )
-    ##############Deleting a genre##############
+    #Deleting a genre
     Genre.objects.filter(name="Action").delete()
-    ##################Deleting a name###################
+    #Deleting a name
     Actor.objects.filter(first_name="Scarlett").delete()
-    ###############Return sorted QuerySet###############
+    #Return sorted QuerySet
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
