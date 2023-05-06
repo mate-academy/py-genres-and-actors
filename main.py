558966182fcec4c2e95473e1500ee5058c75ec7e
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2023-05-06 14:58:07 +0300

    'Solution'

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Will", last_name="Smith")
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
         first_name="George", last_name="Klooney"
@@ -22,6 +23,7 @@ def main() -> QuerySet:
     Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
+
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
