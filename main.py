9a932383136e8af188d1bb2d89daa96eaa51e296
Halyna Petrova <1208petrova@gmail.com>
2023-05-23 14:54:17 +0300

    'Solution2'

--- main.py
+++ main.py
@@ -27,4 +27,3 @@ def main() -> QuerySet:
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
