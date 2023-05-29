b159b4fd26c337cecb3cd06b326d8bf478c9603b
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-29 22:36:42 +0300

    solution

--- main.py
+++ main.py
@@ -2,6 +2,65 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Actor, Genre
+
 
 def main() -> QuerySet:
-    pass
+    genres = ["Western", "Action", "Dramma"]
+    actor_data = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+    Genre.objects.bulk_create(
+        [
+            Genre(name=genre)
+            for genre in genres
+        ]
+    )
+    Actor.objects.bulk_create(
+        [
+            Actor(
+                first_name=actor[0],
+                last_name=actor[1],
+            )
+            for actor in actor_data
+        ]
+    )
+    # for genre in genres:
+    #     Genre.objects.create(name=genre)
+    # for actor in actor_data:
+    #     Actor.objects.create(
+    #         first_name=actor[0],
+    #         last_name=actor[1],
+    #     )
+    Genre.objects.filter(
+        name="Dramma"
+    ).update(name="Drama")
+    Actor.objects.filter(
+        last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves",
+    ).update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
+    Genre.objects.filter(
+        name="Action"
+    ).delete()
+    Actor.objects.filter(
+        first_name="Scarlett"
+    ).delete()
+    actors_ln_smith = Actor.objects.filter(
+        last_name="Smith"
+    ).order_by("first_name")
+    return actors_ln_smith
+
+
+if __name__ == "__main__":
+    main()
