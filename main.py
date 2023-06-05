1f2cacaee94476146d3a35c6e7205fb21e2f0052
Oleksandr <@gmail.com>
2023-06-05 10:03:33 +0300

    'Fixed2'

--- main.py
+++ main.py
@@ -20,9 +20,7 @@ def main() -> QuerySet:
     for actor in actor_list:
         Actor.objects.create(first_name=actor[0], last_name=actor[1])
 
-    Genre.objects.filter(
-        name="Dramma"
-    ).update(name="Drama")
+    Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
         first_name="George",
         last_name="Klooney"
@@ -32,12 +30,8 @@ def main() -> QuerySet:
         last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
 
-    Genre.objects.filter(
-        name="Action"
-    ).delete()
-    Actor.objects.filter(
-        first_name="Scarlett"
-    ).delete()
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
