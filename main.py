bf2031f43c1f67a387cb651123fe13bc88f71019
yuro <yuro740@gmail.com>
2023-06-19 18:12:08 +0300

    Solution changed \"\

--- main.py
+++ main.py
@@ -12,21 +12,18 @@ def main() -> QuerySet:
         ("Scarlett", "Keegan"),
         ("Will", "Smith"),
         ("Jaden", "Smith"),
-        ("Scarlett", "Johansson")
+        ("Scarlett", "Johansson"),
     ]
     for genre in genres:
         Genre.objects.create(name=genre)
     for name, surname in actors:
         Actor.objects.create(first_name=name, last_name=surname)
 
-    Actor.objects.filter(
-        last_name="Klooney"
-    ).update(last_name="Clooney")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
 
-    Actor.objects.filter(
-        first_name="Kianu",
-        last_name="Reaves"
-    ).update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
