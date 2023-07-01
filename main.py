31cfb5dfdde8fc0a513f6a1cc861c0888456ddc6
stepband <vladshvik@gmail.com>
2023-07-01 22:05:18 +0300

    added models for actors and genres and created names of actors and some genres

--- main.py
+++ main.py
@@ -21,12 +21,14 @@ def main() -> QuerySet:
         Actor(first_name="Scarlett", last_name="Johansson"),
     ])
 
-    Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reeves").update(first_name="Keanu", last_name="Reeves")
+    Genre.objects.filter(name="Dramma")\
+        .update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney")\
+        .update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reeves")\
+        .update(first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
