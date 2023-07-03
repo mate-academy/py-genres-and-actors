a58e1d38c530460838cc6e7378269297e90788f5
Tarasidze <dtar84@ukr.net>
2023-07-03 13:50:54 +0300

    Solution 1.04

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ def main() -> QuerySet:
 
     genres = ["Western", "Action", "Dramma"]
     actors = [
-        ("George", "Klooney"), ("Kianu", "Reaves"), ("Scarlett", "Keegan"),
-        ("Will", "Smith"), ("Jaden", "Smith"), ("Scarlett", "Johansson")
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
     ]
 
     for genre in genres:
@@ -22,12 +26,14 @@ def main() -> QuerySet:
             last_name=last_name_
         )
 
-    Genre.objects.filter(name="Dramma",).update(name="Drama")
-    Actor.objects.filter(last_name="Klooney",).update(last_name="Clooney")
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(
         first_name="Kianu",
         last_name="Reaves"
-    ).update(first_name="Keanu", last_name="Reeves")
+    ).update(
+        first_name="Keanu",
+        last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
