ca1904ec68bbe12c5a847879d30d66969909924a
Erik Agayan <erik.agayan1@gmail.com>
2023-07-06 11:03:13 +0300

    Solution #5

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
-def main() -> QuerySet:
+def create_genres() -> None:
     genres = [
         ("Western",),
         ("Action",),
         ("Dramma",),
     ]
 
+    for genre in genres:
+        Genre.objects.create(name=genre[0])
+
+
+def create_actors() -> None:
     actors = [
         ("George", "Klooney"),
         ("Kianu", "Reaves"),
@@ -20,38 +25,37 @@ def main() -> QuerySet:
         ("Scarlett", "Johansson"),
     ]
 
-    for genre in genres:
-        Genre.objects.create(
-            name=genre[0]
-        )
-
     for actor in actors:
-        Actor.objects.create(
-            first_name=actor[0],
-            last_name=actor[1]
-        )
+        Actor.objects.create(first_name=actor[0], last_name=actor[1])
 
-    Genre.objects.filter(
-        name="Dramma",
-    ).update(name="Drama")
 
-    Actor.objects.filter(
-        last_name="Klooney",
-    ).update(last_name="Clooney")
+def update_genre_name() -> None:
+    Genre.objects.filter(name="Dramma").update(name="Drama")
 
-    Actor.objects.filter(
-        first_name="Kianu",
-        last_name="Reaves"
-    ).update(first_name="Keanu", last_name="Reeves")
 
-    Genre.objects.filter(
-        name="Action",
-    ).delete()
+def update_actor_name() -> None:
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves"
+    )
 
-    Actor.objects.filter(
-        first_name="Scarlett",
-    ).delete()
 
-    return Actor.objects.filter(
-        last_name="Smith"
-    ).order_by("first_name")
+def delete_genre_and_actors() -> None:
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+
+def get_filtered_actors() -> QuerySet:
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+
+def main() -> QuerySet:
+    create_genres()
+    create_actors()
+
+    update_genre_name()
+    update_actor_name()
+
+    delete_genre_and_actors()
+
+    return get_filtered_actors()
