e19a940162ad003ac209009b86c1c9159d5d6696
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-08-04 22:06:30 +0300

    models

--- db/models.py
+++ db/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __repr__(self) -> str:
+        return f"<{__class__.__name__}: {self.name}>"
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __repr__(self) -> str:
+        return f"<{__class__.__name__}: {self.first_name} {self.last_name}>"
