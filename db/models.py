2b1fca69a346b5e37f4456066a74166869a5c599
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-08-05 22:02:09 +0300

    flake8

--- db/models.py
+++ db/models.py
@@ -13,4 +13,5 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=255)
 
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__}: {self.first_name} {self.last_name}>"
+        return (f"<{self.__class__.__name__}: "
+                f"{self.first_name} {self.last_name}>")
