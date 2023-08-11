1b4c77e96638f3cb6f48eee363bd4d15746146e5
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-08-11 17:30:51 +0300

    Implemented Genres-and-actors task

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Actor, Genre
+
 
 def main() -> QuerySet:
-    pass
+    genres_base = ["Western", "Action", "Dramma"]
+    actors_base = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+
+    for genre in genres_base:
+        Genre.objects.create(name=genre)
+
+    for item_first, item_second in actors_base:
+        Actor.objects.create(first_name=item_first, last_name=item_second)
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(
+        last_name__contains="Smith"
+    ).order_by("first_name")
