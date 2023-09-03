86de55c03b876e5675dff94ad753e82562767cf4
Max Katkalov <makskatkalov@gmail.com>
2023-09-03 13:20:14 +0300

    solution done

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Actor, Genre
+
+
+objects_to_create = {
+    "geners": ("Western", "Action", "Dramma"),
+    "actors_names": (
+        "George",
+        "Kianu",
+        "Scarlett",
+        "Will",
+        "Jaden",
+        "Scarlett",
+    ),
+    "actors_last_names": (
+        "Klooney",
+        "Reaves",
+        "Keegan",
+        "Smith",
+        "Smith",
+        "Johansson",
+    ),
+}
 
 
 def main() -> QuerySet:
-    pass
+    for f_name, l_name in zip(
+        objects_to_create["actors_names"],
+        objects_to_create["actors_last_names"],
+    ):
+        Actor.objects.create(first_name=f_name, last_name=l_name)
+    for genre_name in objects_to_create["geners"]:
+        Genre.objects.create(name=genre_name)
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(
+        last_name="Clooney"
+    )
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves"
+    )
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+
+if __name__ == "__main__":
+    main()
