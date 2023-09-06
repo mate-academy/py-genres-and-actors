6e3307f898568f004a86a775802c3bb81a89b9b8
Inna Isareva <inna8110302@gmail.com>
2023-09-06 17:50:14 +0300

    implement py-genres-and-actors v2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main() -> QuerySet:
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
 
-    Actor.objects.create(first_name="George ", last_name="Klooney")
+    Actor.objects.create(first_name="George", last_name="Klooney")
     Actor.objects.create(first_name="Kianu", last_name="Reaves")
     Actor.objects.create(first_name="Scarlett", last_name="Keegan")
     Actor.objects.create(first_name="Will", last_name="Smith")
