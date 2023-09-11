de6886b555b7ddd7092f44174a79eb79c2b8e63d
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-09-11 15:29:22 +0300

    'Solution'

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def main() -> QuerySet:
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(last_name="Smith").order_by("first_name").all()
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
