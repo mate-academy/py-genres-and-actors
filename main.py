1764dc8415df89f1dbebc15f7b60f50a8d8ff125
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-09-11 14:26:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ def main() -> QuerySet:
     ]
 
     for genre in genres:
-        Genre.objects.create(name=genre,)
+        Genre.objects.create(name=genre)
 
     for name, surname in actors:
         Actor.objects.create(first_name=name, last_name=surname)
 
     Genre.objects.filter(
-        name="Dramma",
+        name="Dramma"
     ).update(name="Drama")
 
     Actor.objects.filter(
@@ -34,7 +34,7 @@ def main() -> QuerySet:
         last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
 
-    Genre.objects.filter(name="Action",).delete()
-    Actor.objects.filter(first_name="Scarlett",).delete()
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(last_name="Smith",).order_by("first_name",)
+    return Actor.objects.filter(last_name="Smith",).order_by("first_name")
