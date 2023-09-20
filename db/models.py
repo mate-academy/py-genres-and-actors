481a5227705c60ee34e615ed953042b744e7e7bd
Alex Pelevin <ya.pelevin1999@gmail.com>
2023-09-20 16:47:04 +0300

    fix after review

--- db/models.py
+++ db/models.py
@@ -1,10 +1,19 @@
+from typing import List, Dict
+
 from django.db import models
 
 
-class Genre(models.Model):
+class BaseModel:
+    @classmethod
+    def create(cls, items: List[Dict]) -> None:
+        items = [cls(**item) for item in items]
+        cls.objects.bulk_create(items)
+
+
+class Genre(BaseModel, models.Model):
     name = models.CharField(max_length=255)
 
 
-class Actor(models.Model):
+class Actor(BaseModel, models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
--- main.py
+++ main.py
@@ -5,29 +5,35 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    # Create objects
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    Genre.create(
+        [
+            {"name": "Western"},
+            {"name": "Action"},
+            {"name": "Dramma"},
+        ]
+    )
 
-    # Update objects
+    Actor.create(
+        [
+            {"first_name": "George", "last_name": "Klooney"},
+            {"first_name": "Kianu", "last_name": "Reaves"},
+            {"first_name": "Scarlett", "last_name": "Keegan"},
+            {"first_name": "Will", "last_name": "Smith"},
+            {"first_name": "Jaden", "last_name": "Smith"},
+            {"first_name": "Scarlett", "last_name": "Johansson"}
+        ]
+    )
+
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
         first_name="George",
         last_name="Klooney"
-    ).update(last_name="Clooney")
+    ).update(last_name = "Clooney")
     Actor.objects.filter(
         first_name="Kianu",
         last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
 
-    # Delete objects
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
