406e5ef72702e75a0f0e1cf8d9630ecd923956e4
Artem <hartem472@gmail.com>
2023-09-25 20:06:25 +0200

    Solution

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def main() -> QuerySet:
         ("Scarlett", "Johansson"),
     ]
 
-    for actor in actors:
+    for first_name, last_name in actors:
         Actor.objects.create(
-            first_name=actor[0],
-            last_name=actor[1]
+            first_name=first_name,
+            last_name=last_name
         )
 
     Genre.objects.filter(
--- settings.py
+++ settings.py
@@ -12,18 +12,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        "TEST": {
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        },
-    },
+    }
 }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
 
 USE_TZ = False
 
