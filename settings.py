8a2733137dd32e059ba38bb9e035fd60cd95258b
Artem <hartem472@gmail.com>
2023-09-25 17:16:21 +0200

    Solution

--- settings.py
+++ settings.py
@@ -9,11 +9,21 @@ SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        },
+    },
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 
 USE_TZ = False
 
