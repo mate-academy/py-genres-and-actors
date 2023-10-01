2eeef335753d27ca907d95717863af675388979e
serhii_loboda <serhiy.loboda@gmail.com>
2023-10-01 12:44:15 +0300

    Solution: Genres & Actors.

--- db/models.py
+++ db/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.db.models import CharField
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
-    def __str__(self) -> str:
-        return f"{self.name}"
+    def __str__(self) -> CharField:
+        return self.name
 
 
 class Actor(models.Model):
