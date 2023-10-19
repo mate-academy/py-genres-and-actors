071348c6419dcd0ce26ddb970c285633301787a1
Nadiia <nadiia.developer777@gmail.com>
2023-10-19 21:46:59 +0300

    Solution-two-py-genres-and-actors

--- main.py
+++ main.py
@@ -18,32 +18,24 @@ def main() -> QuerySet:
         Actor(first_name="Scarlett", last_name="Keegan"),
         Actor(first_name="Will", last_name="Smith"),
         Actor(first_name="Jaden", last_name="Smith"),
-        Actor(first_name="Scarlett", last_name="Johansson")
+        Actor(first_name="Scarlett", last_name="Johansson"),
     ]
 
     Actor.objects.bulk_create(actors)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
-    Actor.objects.filter(
-        first_name="George",
-        last_name="Klooney"
-    ).update(last_name="Clooney")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(
+        last_name="Clooney"
+    )
 
-    Actor.objects.filter(
-        first_name="Kianu",
-        last_name="Reaves"
-    ).update(
-        first_name="Keanu",
-        last_name="Reeves")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
 
-    actors = Actor.objects.all()
-
-    for actor in actors:
-        if actor.first_name == "Scarlett":
-            actor.delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     queryset = Actor.objects.filter(last_name="Smith").order_by("first_name")
 
