6f93c07fa79ce07e2bef0109b8b628045d0fd46d
Nite NSD <nsd7night@gmail.com>
2023-10-22 20:09:17 +0300

    Solution_genres_and_actors

--- main.py
+++ main.py
@@ -2,6 +2,40 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Actor, Genre
+
 
 def main() -> QuerySet:
-    pass
+    genre_tuple = (
+        "Western",
+        "Action",
+        "Dramma",
+    )
+    actors_tuple = (
+        "George Klooney",
+        "Kianu Reaves",
+        "Scarlett Keegan",
+        "Will Smith",
+        "Jaden Smith",
+        "Scarlett Johansson",
+    )
+
+    for genre in genre_tuple:
+        Genre.objects.create(name=genre)
+
+    for actor in actors_tuple:
+        first_name, last_name = actor.split(" ")
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name
+        )
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu",
+                                                    last_name="Reeves")
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
