571bd255b3da319873565090264f51c4b3d7c626
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-10-22 21:34:43 +0300

    task completed(1)

--- main.py
+++ main.py
@@ -18,10 +18,16 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Keanu", last_name="Reeves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Keanu",
+        last_name="Reeves").update(
+        first_name="Keanu",
+        last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett")
 
-    return Actor.objects.filter(last_name="Smith").order_by('first_name')
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
