b15e5e1e4d1e1e43e229dbef5717c30f9c1b2aac
sakhaline <alexandra.ivva@gmail.com>
2023-11-02 19:53:15 +0200

    Solution

---
+++ data.py
@@ -0,0 +1,27 @@
+GENRES = ["Western", "Action", "Dramma"]
+ACTORS = [
+    {
+        "first_name": "George",
+        "last_name": "Klooney"
+    },
+    {
+        "first_name": "Kianu",
+        "last_name": "Reaves"
+    },
+    {
+        "first_name": "Scarlett",
+        "last_name": "Keegan"
+    },
+    {
+        "first_name": "Will",
+        "last_name": "Smith"
+    },
+    {
+        "first_name": "Jaden",
+        "last_name": "Smith"
+    },
+    {
+        "first_name": "Scarlett",
+        "last_name": "Johansson"
+    }
+]
--- main.py
+++ main.py
@@ -2,6 +2,37 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+from data import GENRES, ACTORS
+
 
 def main() -> QuerySet:
-    pass
+    for genre in GENRES:
+        Genre.objects.create(
+            name=genre
+        )
+
+    for actor in ACTORS:
+        Actor.objects.create(
+            first_name=actor["first_name"],
+            last_name=actor["last_name"]
+        )
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+
+if __name__ == "__main__":
+    main()
