9cd1878f9002ab0f4521b8b89723ffe10cb98e37
Kostia <kostia3507@gmail.com>
2023-11-04 16:09:37 +0200

    using tuples instead of a file

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ from db.models import Actor, Genre
 
 
 def main() -> QuerySet:
-    with open("data.txt", "r") as f:
-        lines = f.read().splitlines()
+    genres = ("Western", "Action", "Dramma")
+    actors = (
+        "George Klooney",
+        "Kianu Reaves",
+        "Scarlett Keegan",
+        "Will Smith",
+        "Jaden Smith",
+        "Scarlett Johansson"
+    )
 
-    for line in lines:
-        data = line.split(" ")
+    for genre in genres:
+        Genre.objects.create(name=genre)
 
-        kind_of_data = data[0]
-
-        if kind_of_data == "genre":
-            genre = data[1]
-            Genre.objects.create(name=genre)
-        else:
-            first_name, last_name = data[1], data[2]
-            Actor.objects.create(first_name=first_name, last_name=last_name)
+    for actor in actors:
+        actor_data = actor.split(" ")
+        first_name, last_name = actor_data
+        Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
+    Genre.objects.filter(name="Action").delete()
+
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
         first_name="Keanu", last_name="Reeves"
     )
-    Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
-
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
