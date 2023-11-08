015c8b7ce3a90c159b9ef1b69d9b9159c17bb948
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-11-08 18:32:52 +0200

    Rewrite list comprehension

--- main.py
+++ main.py
@@ -16,24 +16,29 @@ def main() -> QuerySet:
         ("Jaden", "Smith"),
         ("Scarlett", "Johansson")
     ]
-    [Actor.objects.create(first_name=actor[0], last_name=actor[1])
-     for actor in actors_list]
+    [Actor.objects.create(first_name=first_name, last_name=last_name)
+     for first_name, last_name in actors_list]
+
     Genre.objects.filter(
         name="Dramma"
     ).update(name="Drama")
+
     Actor.objects.filter(
         last_name="Klooney"
     ).update(last_name="Clooney")
+
     Actor.objects.filter(
         first_name="Kianu"
     ).update(first_name="Keanu", last_name="Reeves")
+
     Genre.objects.filter(
         name="Action"
     ).delete()
+
     Actor.objects.filter(
         first_name="Scarlett"
     ).delete()
-    smith_actors = Actor.objects.filter(
+
+    return Actor.objects.filter(
         last_name="Smith"
     ).order_by("first_name")
-    return smith_actors
