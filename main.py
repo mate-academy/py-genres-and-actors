f15858ab64ee60ce174d946650769318f69ca722
Your_Name Your_Surname <your@email>
2023-11-17 23:36:27 +0200

    fix repid in program

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ from db.models import Actor
 from db.models import Genre
 
 
+genre_list = ["Western", "Action", "Dramma"]
+actor_list = [("George", "Klooney"),
+              ("Kianu", "Reaves"),
+              ("Scarlett", "Keegan"),
+              ("Will", "Smith"),
+              ("Jaden", "Smith"),
+              ("Scarlett", "Johansson")
+              ]
+
+
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    for genre in genre_list:
+        Genre.objects.create(name=genre)
+
+    for actor in actor_list:
+        Actor.objects.create(first_name=actor[0], last_name=actor[1])
 
     Genre.objects.filter(
         name="Dramma"
@@ -27,6 +33,7 @@ def main() -> QuerySet:
     Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
+
     Genre.objects.filter(
         name="Action"
     ).delete()
