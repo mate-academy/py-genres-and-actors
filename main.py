5630e0b551b63d90084c32107edfa5e1c694e6ea
Vlad Krit <kritvladislav98@gmail.com>
2023-11-22 15:40:40 +0200

    solution_py-genres-and-actors_3

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from db.models import Genre, Actor
 def main() -> QuerySet:
     genre_list = ["Western", "Action", "Dramma"]
     actors_list = [
-        ["George", "Klooney"],
-        ["Kianu", "Reaves"],
-        ["Scarlett", "Keegan"],
-        ["Will", "Smith"],
-        ["Jaden", "Smith"],
-        ["Scarlett", "Johansson"]
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
     ]
     for item in genre_list:
         Genre.objects.create(name=item)
