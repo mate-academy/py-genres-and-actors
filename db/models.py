d6f01196b077f50c65393b7ef0e9687367d74231
Ivan <mozharivan17@gmail.com>
2023-11-25 17:18:02 +0200

    Add return self.name

--- db/models.py
+++ db/models.py
@@ -5,7 +5,7 @@ class Genre(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
-        return f"Genre: {Genre.name}"
+        return f"Genre: {self.name}"
 
 
 class Actor(models.Model):
@@ -13,4 +13,4 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=63)
 
     def __str__(self) -> str:
-        return f"Actor: {Actor.first_name} {Actor.last_name}"
+        return f"Actor: {self.first_name} {self.last_name}"
