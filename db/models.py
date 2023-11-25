8b6ee46f674afbbad4bab519f5da844220e68f8d
Ivan <mozharivan17@gmail.com>
2023-11-25 06:42:31 +0200

    Add str method

--- db/models.py
+++ db/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return f"Genre: {Genre.name}"
+
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return f"Actor: {Actor.first_name} {Actor.last_name}"
