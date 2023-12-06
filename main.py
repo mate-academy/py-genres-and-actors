2bd9b9dc2cba5a9e6397f693802d25f96ee697ea
Serhii Berdianskyi <zeferot94@gmail.com>
2023-12-06 11:12:34 +0200

    added loops

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    genres_list = ["Western", "Action", "Dramma"]
+    actors_list = [
+        "George Klooney",
+        "Kianu Reaves",
+        "Scarlett Keegan",
+        "Will Smith",
+        "Jaden Smith",
+        "Scarlett Johansson"
+    ]
+    for genre in genres_list:
+        Genre.objects.create(name=genre)
+
+    for actor in actors_list:
+        actor = actor.split()
+        Actor.objects.create(first_name=actor[0], last_name=actor[1])
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
@@ -25,7 +31,8 @@ def main() -> QuerySet:
         last_name="Reaves"
     ).update(
         first_name="Keanu",
-        last_name="Reeves")
+        last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
