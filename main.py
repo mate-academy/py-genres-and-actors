c3b98cd3c69ff1222651d941c7b234c24faf2075
Ostap <ostapbojko04@gmail.com>
2023-12-08 19:45:46 +0200

    py-genres-and-actors(2(fixed))

--- main.py
+++ main.py
@@ -3,34 +3,30 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    genres = [
-        Genre.objects.create(name="Western"),
-        Genre.objects.create(name="Action"),
-        Genre.objects.create(name="Drama"),
+    genre_names = ["Western", "Action", "Drama"]
+    genres = [Genre.objects.create(name=name) for name in genre_names]
+
+    actor_data = [
+        {"first_name": "George", "last_name": "Clooney"},
+        {"first_name": "Keanu", "last_name": "Reeves"},
+        {"first_name": "Scarlett", "last_name": "Keegan"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Jaden", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
     ]
+    [Actor.objects.create(**data) for data in actor_data]
 
-    actors = [
-        Actor.objects.create(first_name="George", last_name="Clooney"),
-        Actor.objects.create(first_name="Keanu", last_name="Reeves"),
-        Actor.objects.create(first_name="Scarlett", last_name="Keegan"),
-        Actor.objects.create(first_name="Will", last_name="Smith"),
-        Actor.objects.create(first_name="Jaden", last_name="Smith"),
-        Actor.objects.create(first_name="Scarlett", last_name="Johansson"),
-    ]
-
-    genres[2].name = "Drama"
-    actors[0].last_name = "Clooney"
-    actors[1].first_name = "Keanu"
-    actors[1].last_name = "Reeves"
-
-    for obj in genres + actors:
-        obj.save()
+    Genre.objects.filter(name="Drama").update(name="Drama")
+    (Actor.objects.filter(last_name="Clooney")
+     .update(last_name="Clooney"))
+    (Actor.objects.filter(first_name="Keanu", last_name="Reeves")
+     .update(first_name="Keanu", last_name="Reeves"))
 
     genres[1].delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    smith_actors = Actor.objects.filter(
-        last_name="Smith").order_by("first_name")
+    smith_actors = (Actor.objects.filter(last_name="Smith")
+                    .order_by("first_name"))
     for actor in smith_actors:
         print(f"{actor.first_name} {actor.last_name}")
 
