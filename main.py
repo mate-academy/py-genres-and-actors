b15a28a6323d1b592bac2b590f54b5213b04e6ca
Nataliia Povashevych <interchanal@ukr.net>
2023-12-20 08:26:10 +0200

    Solution_flake2

--- main.py
+++ main.py
@@ -24,8 +24,11 @@ def main() -> QuerySet:
     # update
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
-                         first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves")
 
     # delete
     Genre.objects.filter(name="Action").delete()
