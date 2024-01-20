b0174f130efa459edfcf821983e3ff546546a9e1
Denys Sidorov <zondayttop1ggg@gmail.com>
2024-01-20 19:57:01 +0200

    Solution 2

--- main.py
+++ main.py
@@ -5,21 +5,26 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    genre_action = Genre.objects.create(name="Action")
-    genre_drama = Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
-    genre_drama.name = "Drama"
-    genre_drama.save()
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu",
-                                                    last_name="Reeves")
-    genre_action.delete()
+    genres_data = ["Western", "Action", "Drama"]
+    actors_data = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    genres = [Genre.objects.create(name=genre_name) for genre_name
+              in genres_data]
+    actors = [Actor.objects.create(first_name=first_name, last_name=last_name) 
+              for first_name, last_name in actors_data]
+    genres[-1].name = "Drama"
+    genres[-1].save()
+    actors[0].last_name = "Clooney"
+    actors[1].first_name = "Keanu"
+    actors[1].last_name = "Reeves"
+    [actor.save() for actor in actors[:2]]
+    genres[1].delete()
     Actor.objects.filter(first_name="Scarlett").delete()
     queryset = Actor.objects.filter(last_name="Smith").order_by("first_name")
     return queryset
