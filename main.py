695ed420e51c43cd40ecc8fae2c58163f33281ca
Vlad <mr.darmstadtium@gmail.com>
2024-01-22 20:50:55 +0100

    pass all tests

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main() -> QuerySet:
         Genre(name="Action"),
         Genre(name="Dramma")
     ]
-    Genre.object.bulk_create(to_create_genres)
+    Genre.objects.bulk_create(to_create_genres)
 
     to_create_actors = [
         Actor(first_name="George", last_name="Klooney"),
@@ -24,35 +24,35 @@ def main() -> QuerySet:
         Actor(first_name="Jaden", last_name="Smith"),
         Actor(first_name="Scarlett", last_name="Johansson")
     ]
-    Actor.object.bulk_create(to_create_actors)
+    Actor.objects.bulk_create(to_create_actors)
 
     # Update
 
-    Genre.object.filter(
+    Genre.objects.filter(
         name="Dramma"
     ).update(name="Drama")
 
-    Actor.object.filter(
+    Actor.objects.filter(
         last_name="Klooney"
     ).update(last_name="Clooney")
 
-    Actor.object.filter(
+    Actor.objects.filter(
         first_name="Kianu",
         last_name="Reaves"
     ).update(last_name="Reeves", first_name="Keanu")
 
     # Delete
 
-    Genre.object.filter(
+    Genre.objects.filter(
         name="Action"
     ).delete()
 
-    Actor.object.filter(
+    Actor.objects.filter(
         first_name="Scarlett"
     ).delete()
 
     # Get(Return)
 
-    Actor.object.get(
+    return Actor.objects.filter(
         last_name="Smith"
-    ).order_by()
+    ).order_by("first_name")
