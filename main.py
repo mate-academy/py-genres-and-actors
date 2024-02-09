e9cd62940b6fe775781b29e611b375ad4d61726d
unknown <artem.sapiens@gmail.com>
2024-02-09 16:55:31 +0200

    Fix-flake8-problems

--- main.py
+++ main.py
@@ -17,15 +17,28 @@ def main() -> QuerySet:
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
     Actor.objects.filter(
-        first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+        first_name="George", last_name="Klooney"
+    ).update(
+        last_name="Clooney"
+    )
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(
+        first_name="Keanu", last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
 
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    smith_actors = Actor.objects.filter(last_name="Smith").order_by('first_name')
+    smith_actors = Actor.objects.filter(
+        last_name="Smith"
+    ).order_by(
+        "first_name"
+    )
+
     return smith_actors
+
 
 if __name__ == "__main__":
     print(main())
--- mock_data.py
+++ mock_data.py
@@ -7,4 +7,4 @@ actors = [
         ("Will", "Smith"),
         ("Jaden", "Smith"),
         ("Scarlett", "Johansson")
-    ]
+]
