c8f4c67c85cd3ceb0916e239bbf22255885f18c4
unknown <artem.sapiens@gmail.com>
2024-02-09 16:59:19 +0200

    Fix-flake8-problems2

--- mock_data.py
+++ mock_data.py
@@ -1,10 +1,10 @@
 genres = {"Western", "Action", "Dramma"}
 
 actors = [
-        ("George", "Klooney"),
-        ("Kianu", "Reaves"),
-        ("Scarlett", "Keegan"),
-        ("Will", "Smith"),
-        ("Jaden", "Smith"),
-        ("Scarlett", "Johansson")
+    ("George", "Klooney"),
+    ("Kianu", "Reaves"),
+    ("Scarlett", "Keegan"),
+    ("Will", "Smith"),
+    ("Jaden", "Smith"),
+    ("Scarlett", "Johansson")
 ]
