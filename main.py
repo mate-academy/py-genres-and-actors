3021c84fc6ab5a69365b1d98827bb78383cb97f8
Kyrylo Storozhenko <busterakel@gmail.com>
2024-02-11 11:21:08 +0200

    Solution

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# main.py
 import init_django_orm  # noqa: F401
+
 from django.db.models import QuerySet
+
 from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    genres = [("Western",), ("Dramma",)]
+    genres = ["Western", "Dramma"]
     actors = [
         ("George", "Klooney"),
         ("Kianu", "Reaves"),
@@ -15,18 +16,24 @@ def main() -> QuerySet:
         ("Scarlett", "Johansson"),
     ]
 
-    for name, in genres:
+    for name in genres:
         Genre.objects.create(name=name)
 
     for first_name, last_name in actors:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
-    (Genre.objects.filter(name="Dramma").
-     update(name="Drama"))
-    (Actor.objects.filter(first_name="George", last_name="Klooney").
-     update(last_name="Clooney"))
-    (Actor.objects.filter(first_name="Kianu", last_name="Reaves").
-     update(first_name="Keanu", last_name="Reeves"))
+    (
+        Genre.objects.filter(name="Dramma").
+        update(name="Drama")
+    )
+    (
+        Actor.objects.filter(first_name="George", last_name="Klooney").
+        update(last_name="Clooney")
+    )
+    (
+        Actor.objects.filter(first_name="Kianu", last_name="Reaves").
+        update(first_name="Keanu", last_name="Reeves")
+    )
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
