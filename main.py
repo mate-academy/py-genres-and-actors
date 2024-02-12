b89d30294394da4c5f3912d609c58b39278ea18a
el chibuk <chebukin404@gmail.com>
2024-02-12 09:48:40 +0200

    fixed filtration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+
 from db.models import Genre, Actor
 
 
@@ -25,7 +26,9 @@ def main() -> QuerySet:
         )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney"
+    ).update(last_name="Clooney")
     Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
