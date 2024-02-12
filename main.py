9e6d894a646204953d2212ec863a94d18da08664
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-02-12 12:40:18 +0200

    Fixed according to comments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import init_django_orm  # noqa: F401
 from django.db.models import QuerySet
+
 from db.models import Genre, Actor
 
 
@@ -22,7 +23,9 @@ def main() -> QuerySet:
 
     Genre.objects.filter(name="Drama").update(name="Drama")
 
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="George",
+                         last_name="Klooney"
+                         ).update(last_name="Clooney")
     Actor.objects.filter(
         first_name="Kianu",
         last_name="Reeves"
