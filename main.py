455bcab194013c7dc0a44e6583a698272ac40c56
Kyrylo Storozhenko <busterakel@gmail.com>
2024-02-12 09:49:07 +0200

    fixed line brakes

--- main.py
+++ main.py
@@ -22,19 +22,13 @@ def main() -> QuerySet:
     for first_name, last_name in actors:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
-    (
-        Genre.objects.filter(name="Dramma").
-        update(name="Drama")
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(
+        last_name="Clooney"
     )
-    (
-        Actor.objects.filter(first_name="George", last_name="Klooney").
-        update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves"
     )
-    (
-        Actor.objects.filter(first_name="Kianu", last_name="Reaves").
-        update(first_name="Keanu", last_name="Reeves")
-    )
-
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
