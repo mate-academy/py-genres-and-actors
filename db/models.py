dfdf4eb5819d238bfd52818e95d562d702b9979d
Sofia Zholud <szholuds@gmail.com>
2024-02-20 16:31:06 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -10,5 +10,5 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
