a0622822a7ff61de138ac2269a2bb44411bff466
unknown <oleksandrlysanets@gmail.com>
2024-02-22 12:17:00 +0200

    Solution

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from django.db.models import QuerySet
 from db.models import Actor, Genre
 
 
-def main() -> QuerySet:
+def main() -> QuerySet[Actor]:
     genres = ["Western", "Action", "Dramma"]
 
     for ganre in genres:
