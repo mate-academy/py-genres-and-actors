e96a8de4c83f18a22cdb0bad4fc2fd095bf80e18
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-02-23 00:20:50 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,16 @@
-from django.db import models
+from django.db.models import Model, CharField
+
+
+class Genre(Model):
+    name = CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"Genre with name: {self.name}"
+
+
+class Actor(Model):
+    first_name = CharField(max_length=255)
+    last_name = CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"Actor: {self.first_name} {self.last_name}"
--- settings.py
+++ settings.py
@@ -10,8 +10,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cinema",
+        "USER": "postgres",
+        "PASSWORD": "15vktelefon",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
