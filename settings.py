15540c5a1870d6f79757b5bf3a8342e74609cd1f
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-02-23 23:58:19 +0200

    Fix

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "cinema",
         "USER": "postgres",
-        "PASSWORD": "",
+        "PASSWORD": "postgres",
         "HOST": "localhost",
         "PORT": "5432",
     }
