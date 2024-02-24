a1f79e88443cd25d180d528108d44cd548be9acb
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-02-24 12:36:19 +0200

    implement-py-genres-and-actors with cycle

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from db.models import Actor, Genre
 
 def main() -> QuerySet:
     # CREATE
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansonn")
+    genres_to_create = ["Western", "Action", "Dramma"]
+    for element in genres_to_create:
+        Genre.objects.create(name=element)
+
+    actors_to_create = {"first_name": ["George", "Kianu", "Scarlett",
+                                       "Will", "Jaden", "Scarlett"],
+                        "last_name": ["Klooney", "Reaves", "Keegan",
+                                      "Smith", "Smith", "Johansson"]}
+    for first_name, last_name in zip(actors_to_create["first_name"],
+                                     actors_to_create["last_name"]):
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
     # UPDATE
     Genre.objects.filter(name="Dramma", ).update(name="Drama")
     (Actor.objects.filter(first_name="George", last_name="Klooney", )
