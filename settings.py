a6cc69912b9378c795ee46746b5aee6fd2ae40c2
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2024-03-02 06:21:44 +0200

    Solution

--- settings.py
+++ settings.py
@@ -6,8 +6,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 
-
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
