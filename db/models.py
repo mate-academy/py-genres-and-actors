e8cc5cd91c998016dd690b96646aa695a24ac649
Olena Boldysheva <adv55577@gmail.com>
2024-03-04 15:11:02 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Genre"
+        verbose_name_plural = "Genres"
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Actor"
+        verbose_name_plural = "Actors"
