b8db75bacbf6191815f9ee8e18cdffad5dfebbc5
Maksim <sma4no.7@gmail.com>
2024-03-12 17:41:21 +0100

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre
+from db.models import Actor
 
 
 def main() -> QuerySet:
-    pass
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Johansson"
+    )
+
+if __name__ == "__main__":
+    main()
