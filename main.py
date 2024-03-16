45073f06ec0f820c57a3d3186bb59080a0c9febc
DenZatolokin <denzatolokin@gmail.com>
2024-03-16 20:39:14 +0200

    Done

--- main.py
+++ main.py
@@ -6,50 +6,36 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    action_genre = Genre.objects.create(name="Action")
-    drama_genre = Genre.objects.create(name="Drama")
+    genre_names = ["Western", "Action", "Dramma"]
+    actors_data = [
+        {"first_name": "George", "last_name": "Klooney"},
+        {"first_name": "Kianu", "last_name": "Reaves"},
+        {"first_name": "Scarlett", "last_name": "Keegan"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Jaden", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
+    ]
+    for genre_name in genre_names:
+        Genre.objects.create(name=genre_name)
 
-    george_clooney = Actor.objects.create(
-        first_name="George",
-        last_name="Clooney"
-    )
-    keanu_reeves = Actor.objects.create(
-        first_name="Kianu",
-        last_name="Reeves"
-    )
-    scarlett_keegan = Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Keegan"
-    )
-    Actor.objects.create(
-        first_name="Will",
-        last_name="Smith"
-    )
-    Actor.objects.create(
-        first_name="Jaden",
-        last_name="Smith"
-    )
-    scarlett_johansson = Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Johansson"
-    )
+    for actor_data in actors_data:
+        Actor.objects.create(**actor_data)
 
-    drama_genre.name = "Drama"
-    drama_genre.save()
+    (Genre.objects.filter(name="Dramma").
+     update(name="Drama"))
 
-    george_clooney.last_name = "Clooney"
-    george_clooney.save()
+    (Actor.objects.filter(last_name="Klooney").
+     update(last_name="Clooney"))
 
-    keanu_reeves.first_name = "Keanu"
-    keanu_reeves.last_name = "Reeves"
-    keanu_reeves.save()
+    (Actor.objects.filter(first_name="Kianu").
+     update(first_name="Keanu", last_name="Reeves"))
 
-    action_genre.delete()
-    scarlett_keegan.delete()
-    scarlett_johansson.delete()
+    (Genre.objects.
+     filter(name="Action").delete())
 
-    smith_actors = (Actor.objects.
-                    filter(last_name="Smith").
-                    order_by("first_name"))
-    return smith_actors
+    (Actor.objects.
+     filter(first_name="Scarlett").delete())
+
+    return (Actor.objects.
+            filter(last_name="Smith").
+            order_by("first_name"))
