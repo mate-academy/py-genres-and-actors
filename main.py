d01d094f1796514ffc868d8909c7afe20b2e9a62
martyr <vitaliyburkalo81@gmail.com>
2024-03-21 15:31:04 +0200

    Implemented suggested changes

--- main.py
+++ main.py
@@ -6,52 +6,37 @@ from django.db.models import QuerySet
 
 
 def main() -> QuerySet:
-    Genre.objects.create(
-        name="Western",
-    )
-    Genre.objects.create(
-        name="Action",
-    )
-    Genre.objects.create(
-        name="Dramma",
-    )
-    Actor.objects.create(
-        first_name="George",
-        last_name="Klooney",
-    )
-    Actor.objects.create(
-        first_name="Kianu",
-        last_name="Reaves",
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Keegan",
-    )
-    Actor.objects.create(
-        first_name="Will",
-        last_name="Smith",
-    )
-    Actor.objects.create(
-        first_name="Jaden",
-        last_name="Smith",
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Johansson",
-    )
+    genre_names = ["Western", "Action", "Drama"]
+    for name in genre_names:
+        Genre.objects.get_or_create(name=name)
 
-    drama_genre = Genre.objects.get(name="Dramma")
-    drama_genre.name = "Drama"
-    drama_genre.save()
+    actor_info = [
+        {"first_name": "George", "last_name": "Clooney"},
+        {"first_name": "Keanu", "last_name": "Reeves"},
+        {"first_name": "Scarlett", "last_name": "Keegan"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Jaden", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
+    ]
 
-    george = Actor.objects.get(last_name="Klooney")
-    george.last_name = "Clooney"
-    george.save()
+    for actor in actor_info:
+        Actor.objects.get_or_create(**actor)
 
-    keanu = Actor.objects.get(first_name="Kianu")
-    keanu.first_name = "Keanu"
-    keanu.last_name = "Reeves"
-    keanu.save()
+    if Genre.objects.filter(name="Dramma").exists():
+        drama_genre = Genre.objects.get(name="Dramma")
+        drama_genre.name = "Drama"
+        drama_genre.save()
+
+    if Actor.objects.filter(last_name="Klooney").exists():
+        george = Actor.objects.get(last_name="Klooney")
+        george.last_name = "Clooney"
+        george.save()
+
+    if Actor.objects.filter(first_name="Kianu").exists():
+        keanu = Actor.objects.get(first_name="Kianu")
+        keanu.first_name = "Keanu"
+        keanu.last_name = "Reeves"
+        keanu.save()
 
     Genre.objects.filter(
         name="Action"
