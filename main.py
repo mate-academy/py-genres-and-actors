f1756a2cff8072f2631d5ea59c258f4f7344f3b7
Artom <artem.umantsev@gmail.com>
2024-03-23 13:42:41 +0200

    'Solution'

--- main.py
+++ main.py
@@ -18,13 +18,17 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(name="Kianu",
+    Actor.objects.filter(first_name="Kianu",
                          last_name="Reaves").update(first_name="Keanu",
                                                     last_name="Reeves")
-    Actor.objects.filter(name="George",
+    Actor.objects.filter(first_name="George",
                          last_name="Klooney").update(last_name="Clooney")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+
+if __name__ == "__main__":
+    print(main())
