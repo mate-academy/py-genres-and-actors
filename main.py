c500d04d75a32d4c9d44b5dc64612208916165c4
Dede <specialized8393@gmail.com>
2024-04-02 17:45:21 +0000

    flake8 passed

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ def main() -> QuerySet:
             name=genre,
         )
 
-    for actor in ("George Klooney", "Kianu Reaves", "Scarlett Keegan", "Will Smith", "Jaden Smith", "Scarlett Johansson"):
+    for actor in (
+            "George Klooney",
+            "Kianu Reaves",
+            "Scarlett Keegan",
+            "Will Smith",
+            "Jaden Smith",
+            "Scarlett Johansson"
+    ):
         Actor.objects.create(
             first_name=actor.split()[0],
             last_name=actor.split()[1],
@@ -18,7 +25,13 @@ def main() -> QuerySet:
     # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu",  last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
     # Delete
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
@@ -26,7 +39,7 @@ def main() -> QuerySet:
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(main())
     print(Genre.objects.all())
     print(Actor.objects.all())
