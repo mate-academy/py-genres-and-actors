2f76c47435cd5a9366bffb4b9474ab552bfaedbf
antonver <anton.verbovyi@gmail.com>
2024-04-03 20:06:26 +0200

    done?

--- main.py
+++ main.py
@@ -6,24 +6,32 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
+    genre_names = ["Western",
+                   "Action",
+                   "Drama"]
+    actor_data = [("George", "Klooney"),
+                  ("Kianu", "Reaves"),
+                  ("Scarlett", "Keegan"),
+                  ("Will", "Smith"),
+                  ("Jaden", "Smith"),
+                  ("Scarlett", "Johansson")]
+
     Genre.objects.bulk_create(
-        [Genre(name="Western"),
-         Genre(name="Action"),
-         Genre(name="Dramma"),
-         ]
+        [Genre(name=name) for name in genre_names]
     )
+
     Actor.objects.bulk_create(
-        [Actor(first_name="George", last_name="Klooney"),
-         Actor(first_name="Kianu", last_name="Reaves"),
-         Actor(first_name="Scarlett", last_name="Keegan"),
-         Actor(first_name="Will", last_name="Smith"),
-         Actor(first_name="Jaden", last_name="Smith"),
-         Actor(first_name="Scarlett", last_name="Johansson")])
+        [Actor(first_name=first_name, last_name=last_name)
+         for first_name, last_name in actor_data]
+    )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George").update(last_name="Clooney")
-    (Actor.objects.filter(first_name="Kianu").
-     update(first_name="Keanu", last_name="Reeves"))
+    Actor.objects.filter(first_name="Kianu").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
+
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
