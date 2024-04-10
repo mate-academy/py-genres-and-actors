e883f0fa5a4b25a57ba9b359dfd33e1e90cfa721
Vitalii <warcied1@gmail.com>
2024-04-10 12:41:03 +0300

    Solution

--- main.py
+++ main.py
@@ -3,30 +3,25 @@ import init_django_orm  # noqa: F401
 from django.db.models import QuerySet
 from db.models import Actor, Genre
 
-
-def get_firstname_lastname_from_full_name(fullname: str) -> dict:
-    data = fullname.split()
-    return {"first_name": data[0], "last_name": data[1]}
-
-
 def main() -> QuerySet:
     genres_to_create = ["Western", "Action", "Dramma"]
-    for curr_item in genres_to_create:
+    for curr_genre in genres_to_create:
         Genre.objects.create(
-            name=curr_item
+            name=curr_genre
         )
 
     actors_to_create = [
-        get_firstname_lastname_from_full_name("George Klooney"),
-        get_firstname_lastname_from_full_name("Kianu Reaves"),
-        get_firstname_lastname_from_full_name("Scarlett Keegan"),
-        get_firstname_lastname_from_full_name("Will Smith"),
-        get_firstname_lastname_from_full_name("Jaden Smith"),
-        get_firstname_lastname_from_full_name("Scarlett Johansson"),
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
     ]
     for actor_data in actors_to_create:
         Actor.objects.create(
-            **actor_data
+            first_name=actor_data[0],
+            last_name=actor_data[1]
         )
 
     Genre.objects.filter(
@@ -34,11 +29,13 @@ def main() -> QuerySet:
     ).update(name="Drama")
 
     Actor.objects.filter(
-        **get_firstname_lastname_from_full_name("George Klooney")
+        first_name="George",
+        last_name="Klooney"
     ).update(last_name="Clooney")
 
     Actor.objects.filter(
-        **get_firstname_lastname_from_full_name("Kianu Reaves")
+        first_name="Kianu",
+        last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
