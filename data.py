6c2b79e827b6e6cb9e1c80506ad2bbe0cb85d21e
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-04-11 08:40:59 +0300

    Fixed according to the comment

---
+++ data.py
@@ -0,0 +1,10 @@
+genres = ["Western", "Action", "Dramma"]
+
+actors = [
+    {"first_name": "George", "last_name": "Klooney"},
+    {"first_name": "Kianu", "last_name": "Reaves"},
+    {"first_name": "Scarlett", "last_name": "Keegan"},
+    {"first_name": "Will", "last_name": "Smith"},
+    {"first_name": "Jaden", "last_name": "Smith"},
+    {"first_name": "Scarlett", "last_name": "Johansson"},
+]
