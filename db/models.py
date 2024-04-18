72bf4d20d5895134ca2122162b0d23de460f9594
kekW <panicua12@gmail.com>
2024-04-18 11:42:07 +0300

    test code with me

--- db/models.py
+++ db/models.py
@@ -4,19 +4,12 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
-    def __repr__(self) -> str:
-        return (
-            f"<{self.__class__.__name__}: "
-            f"{self.name}>"
-        )
-
+    def __str__(self):
+        return f"{self.name}"
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __repr__(self) -> str:
-        return (
-            f"<{self.__class__.__name__}: "
-            f"{self.first_name} {self.last_name}>"
-        )
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
