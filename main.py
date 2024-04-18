828e7ffa678122a13b08cd81b05a92c44ff7a113
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-18 11:11:53 +0200

    added separated functions

--- main.py
+++ main.py
@@ -1,52 +1,74 @@
+from typing import Type
+
+from django.db.models import QuerySet, Model
+
 import init_django_orm  # noqa: F401
-
-from django.db.models import QuerySet
-
 from db.models import Genre, Actor
 
 
+def create(model: Type[Model], values_to_create: dict) -> None:
+    model.objects.create(**values_to_create)
+
+
+def update(
+        model: Type[Model],
+        filter_conditions: dict,
+        update_values: dict
+) -> None:
+    model.objects.filter(**filter_conditions).update(**update_values)
+
+
+def delete(model: Type[Model], filter_conditions: dict) -> None:
+    model.objects.filter(
+        **filter_conditions,
+    ).delete()
+
+
+def get_by_filter(model: Type[Model], filter_conditions: dict) -> QuerySet:
+    return model.objects.filter(**filter_conditions)
+
+
 def main() -> QuerySet:
-    genres = ["Western", "Action", "Dramma"]
-    for genre in genres:
-        Genre.objects.create(
-            name=genre,
-        )
+    for genre in [{"name": "Western"}, {"name": "Action"}, {"name": "Dramma"}]:
+        create(Genre, genre)
 
     actors = [
-        "George Klooney",
-        "Kianu Reaves",
-        "Scarlett Keegan",
-        "Will Smith",
-        "Jaden Smith",
-        "Scarlett Johansson"
+        {
+            "first_name": "George",
+            "last_name": "Klooney"
+        },
+        {
+            "first_name": "Kianu",
+            "last_name": "Reaves"
+        },
+        {
+            "first_name": "Scarlett",
+            "last_name": "Keegan"
+        },
+        {
+            "first_name": "Will",
+            "last_name": "Smith"
+        },
+        {
+            "first_name": "Jaden",
+            "last_name": "Smith"
+        },
+        {
+            "first_name": "Scarlett",
+            "last_name": "Johansson"
+        },
     ]
     for actor in actors:
-        Actor.objects.create(
-            first_name=actor.split()[0],
-            last_name=actor.split()[1]
-        )
+        create(Actor, actor)
 
-    Genre.objects.filter(
-        name="Dramma",
-    ).update(name="Drama")
+    update(Genre, {"name": "Dramma"}, {"name": "Drama"})
+    update(Actor, {"last_name": "Klooney"}, {"last_name": "Clooney"})
+    update(
+        Actor,
+        {"first_name": "Kianu", "last_name": "Reaves"},
+        {"first_name": "Keanu", "last_name": "Reeves"})
 
-    Actor.objects.filter(
-        last_name="Klooney",
-    ).update(last_name="Clooney")
+    delete(Genre, {"name": "Action"})
+    delete(Actor, {"first_name": "Scarlett"})
 
-    Actor.objects.filter(
-        first_name="Kianu",
-        last_name="Reaves"
-    ).update(first_name="Keanu", last_name="Reeves")
-
-    Genre.objects.filter(
-        name="Action",
-    ).delete()
-
-    Actor.objects.filter(
-        first_name="Scarlett",
-    ).delete()
-
-    return Actor.objects.filter(
-        last_name="Smith",
-    ).order_by("first_name")
+    return get_by_filter(Actor, {"last_name": "Smith"}).order_by("first_name")
