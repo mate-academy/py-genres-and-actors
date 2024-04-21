f90ad5764867cc595e62e275438ff0edd5fa778d
Yuliia Dudych <juliadudych@gmail.com>
2024-04-21 15:01:50 +0300

    Fix update method to change last name

--- main.py
+++ main.py
@@ -30,7 +30,10 @@ def main() -> QuerySet:
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu")
+    Actor.objects.filter(first_name="Kianu").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
