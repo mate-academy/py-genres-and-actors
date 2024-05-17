4bb012d333b967acba2a4ecfb460477a75c4fb8a
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-05-17 18:38:28 +0300

    succuses

--- settings.py
+++ settings.py
@@ -1,20 +1,15 @@
-import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: Modify this secret key if using in production!
-SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
-
-DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": BASE_DIR / "db.sqlite",
     }
 }
 
-USE_TZ = False
-
-INSTALLED_APPS = ("db",)
+INSTALLED_APPS = [
+    "db",
+]
