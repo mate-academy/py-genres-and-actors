2785f83c235d47ed681a92444848f4e31923a4d5
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2024-05-17 09:48:31 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"Genre: {self.name}"
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Actor (first name: {self.first_name}, last name: {self.last_name}"
