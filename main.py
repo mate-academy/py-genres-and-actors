bf5efad4e19ef6d7d51a9a2bc5a1763ec9de09c4
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-06-16 17:07:27 +0200

    some other fixes

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import init_django_orm  # noqa: F401
 
-from django.db import connection
+# from django.db import connection
 from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
-def reset_table(table_name: str) -> None:
-    with connection.cursor() as cursor:
-        cursor.execute(f"DELETE FROM '{table_name}';")
-        cursor.execute(
-            f"DELETE FROM sqlite_sequence WHERE name='{table_name}';"
-        )
+# def reset_table(table_name: str) -> None:
+#     with connection.cursor() as cursor:
+#         cursor.execute(f"DELETE FROM '{table_name}';")
+#         cursor.execute(
+#             f"DELETE FROM sqlite_sequence WHERE name='{table_name}';"
+#         )
 
 
 def main() -> QuerySet:
-    reset_table("db_genre")
-    reset_table("db_actor")
+    # reset_table("db_genre")
+    # reset_table("db_actor")
 
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
