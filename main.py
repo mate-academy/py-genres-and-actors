90b1a760ddb2488edab71ea58ff3e22db09a6cb9
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-06-16 15:53:23 +0200

    Solution v2

--- main.py
+++ main.py
@@ -23,5 +23,4 @@ def main() -> QuerySet:
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    smith_actors = Actor.objects.filter(first_name="Smith").order_by("first_name")
-    return smith_actors
+    return Actor.objects.filter(first_name="Smith").order_by("first_name")
