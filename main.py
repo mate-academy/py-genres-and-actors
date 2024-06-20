13063efc284b957789a426fe6378bcb4957d363d
timur <stukantimur811@gmail.com>
2024-06-20 17:51:45 +0300

    created two tables and created smth with objects

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    list_of_creation_genre = ["Western", "Action", "Dramma"]
+    for new_genre in list_of_creation_genre:
+        Genre.objects.create(
+            name=new_genre
+        )
+
+    list_of_creation_actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+    for first_name, last_name in list_of_creation_actors:
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name)
+
+    Genre.objects.filter(
+        name="Dramma"
+    ).update(name="Drama")
+    Actor.objects.filter(
+        first_name="George"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu"
+    ).update(first_name="Keanu", last_name="Reeves")
+
+    Genre.objects.filter(
+        name="Action"
+    ).delete()
+    Actor.objects.filter(
+        first_name="Scarlett"
+    ).delete()
+
+    return Actor.objects.filter(
+        last_name="Smith"
+    ).order_by("first_name")
