1d0219ff22b289633a66e77c15f3d876187ed679
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-06-23 18:41:12 +0300

    update

--- main.py
+++ main.py
@@ -6,33 +6,39 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    # CREATE
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    genres = ["Western", "Action", "Dramma"]
+    for genre in genres:
+        Genre.objects.create(name=genre)
 
-    # UPDATE
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(
-        last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu",
-                         last_name="Reaves").update(
-        first_name="Keanu",
-        last_name="Reeves")
 
-    # Delete
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney"
+    ).update(last_name="Clooney")
+
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+
     Genre.objects.filter(name="Action").delete()
+
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(
-        last_name="Smith"
-    ).order_by("first_name", "last_name")
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
 if __name__ == "__main__":
