43150c1bd90d9d5198cfd1ca26595a685736cfbe
Ira <iramladanovych@gmail.com>
2024-06-26 19:25:54 +0300

    Solution

--- main.py
+++ main.py
@@ -2,33 +2,45 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
-from db.models import Genre, Artist
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    western = Genre.objects.create(name="Western")
-    action = Genre.objects.create(name="Action")
-    drama = Genre.objects.create(name="Dramma")
+    genres = ["Western", "Action", "Dramma"]
+    actors = [
+        {"first_name": "George", "last_name": "Klooney"},
+        {"first_name": "Kianu", "last_name": "Reaves"},
+        {"first_name": "Scarlett", "last_name": "Keegan"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Jaden", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
+    ]
 
-    klooney = Artist.objects.create(first_name="George", last_name="Klooney")
-    reaves = Artist.objects.create(first_name="Kianu", last_name="Reaves")
-    keegan = Artist.objects.create(first_name="Scarlett", last_name="Keegan")
-    smith_will = Artist.objects.create(first_name="Will", last_name="Smith")
-    smith_jaden = Artist.objects.create(first_name="Jaden", last_name="Smith")
-    johansson = Artist.objects.create(first_name="Scarlett", last_name="Johansson")
+    genre_objects = []
+    for genre_name in genres:
+        genre_objects.append(Genre.objects.create(name=genre_name))
 
-    drama.name = "Drama"
-    drama.save()
+    actor_objects = []
+    for actor_data in actors:
+        actor_objects.append(Actor.objects.create(**actor_data))
 
-    klooney.last_name = "Clooney"
-    klooney.save()
+    drama_genre = Genre.objects.get(name="Dramma")
+    drama_genre.name = "Drama"
+    drama_genre.save()
 
-    reaves.first_name = "Keanu"
-    reaves.last_name = "Reaves"
-    reaves.save()
+    klooney_actor = Actor.objects.get(first_name="George", last_name="Klooney")
+    klooney_actor.last_name = "Clooney"
+    klooney_actor.save()
 
-    action.delete()
-    Artist.objects.filter(first_name="Scarlett").delete()
+    reaves_actor = Actor.objects.get(first_name="Kianu", last_name="Reaves")
+    reaves_actor.first_name = "Keanu"
+    reaves_actor.last_name = "Reeves"
+    reaves_actor.save()
 
-    smith_actors = Artist.objects.filter(last_name="Smith").order_by("first_name")
+    Genre.objects.get(name="Action").delete()
+
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    smith_actors = Actor.objects.filter(last_name="Smith") \
+        .order_by("first_name")
     return smith_actors
