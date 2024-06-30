42e4214a7e99b86046567db91e7f0d2e657a8a51
Olena Zaritska <zaritskaos@gmail.com>
2024-06-30 19:37:51 +0300

    delete unnecessary comma

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def main() -> QuerySet:
         ("Scarlett", "Keegan"),
         ("Will", "Smith"),
         ("Jaden", "Smith"),
-        ("Scarlett", "Johansson"),
+        ("Scarlett", "Johansson")
     ]
 
     for genre in genres:
-        Genre.objects.create(name=genre,)
+        Genre.objects.create(name=genre)
 
     for first_name, last_name in actors:
         Actor.objects.create(
@@ -25,14 +25,14 @@ def main() -> QuerySet:
             last_name=last_name
         )
 
-    Genre.objects.filter(name="Dramma",).update(name="Drama")
-    Actor.objects.filter(last_name="Klooney",).update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu",).update(
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu").update(
         first_name="Keanu",
         last_name="Reeves"
     )
 
-    Genre.objects.filter(name="Action",).delete()
-    Actor.objects.filter(first_name="Scarlett",).delete()
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
