1b057d55a5e45fa6eedcb67134bbbf8c544d6788
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-04 13:43:31 +0300

    Solution 04-07-2024 13-43

--- main.py
+++ main.py
@@ -6,4 +6,20 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+
+
+
+
+
+
