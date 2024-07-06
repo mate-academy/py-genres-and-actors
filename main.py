1803cf7f0965e5a7d46d80ae9c4037c58dd4d4f5
VikTOR <dev.psychologist@gmail.com>
2024-07-06 11:50:26 +0300

    Models was created (flake8 done!)

--- main.py
+++ main.py
@@ -23,8 +23,11 @@ def main() -> QuerySet:
     # UPDATE
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="George",
+                         last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu",
+                         last_name="Reaves").update(first_name="Keanu",
+                                                    last_name="Reeves")
 
     # DELETE
 
@@ -33,6 +36,7 @@ def main() -> QuerySet:
 
     # RETURN
 
-    smith_actors = Actor.objects.filter(last_name="Smith").order_by("first_name")
+    smith_actors = Actor.objects.filter(
+        last_name="Smith").order_by("first_name")
 
     return smith_actors
