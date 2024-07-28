8c263804377d85172c76d7ea13fd10a66af8fcde
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-07-28 15:40:50 +0300

    Update main.py
    flake8 app fix

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ def main() -> QuerySet:
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="Kianu").update(
+        first_name="Keanu", last_name="Reeves"
+    )
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
