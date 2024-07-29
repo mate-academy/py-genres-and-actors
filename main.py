c96e3db55b317bd3bf22bc7288b6d07eae28efca
Your_Name Your_Surname <your@email>
2024-07-29 18:55:39 +0300

    сÐclear up кredundant variable

--- main.py
+++ main.py
@@ -32,8 +32,5 @@ def main() -> QuerySet:
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
-    smith_actors = (
-        Actor.objects.filter(last_name="Smith")
-        .order_by("first_name")
-    )
-    return smith_actors
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
