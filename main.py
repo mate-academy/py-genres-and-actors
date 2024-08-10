a6d198adebd866fc465bf25d477b920aa998cb26
Semyon Yerep <yerepsemyon@gmail.com>
2024-08-10 16:19:19 +0300

    Solution

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ def main() -> QuerySet:
     for genre in genres_to_create:
         Genre.objects.create(name=genre)
 
-    actors_to_create = [("George", "Klooney"), ("Kianu", "Reaves"), ("Scarlett", "Keegan"),
-                        ("Will", "Smith"), ("Jaden", "Smith"), ("Scarlett", "Johansson")]
+    actors_to_create = [("George", "Klooney"), ("Kianu", "Reaves"),
+                        ("Scarlett", "Keegan"), ("Will", "Smith"),
+                        ("Jaden", "Smith"), ("Scarlett", "Johansson")]
     for first_name, last_name in actors_to_create:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
     #  UPDATE SECTION -----
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    (Actor.objects.filter(first_name="George", last_name="Klooney")
+     .update(last_name="Clooney"))
+    (Actor.objects.filter(first_name="Kianu", last_name="Reaves")
+     .update(first_name="Keanu", last_name="Reeves"))
 
     #  DELETE SECTION -----
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
     #  RETURN SECTION -----
-    query_to_return = Actor.objects.filter(last_name="Smith").order_by("first_name")
+    query_to_return = (Actor.objects.filter(last_name="Smith")
+                       .order_by("first_name"))
 
     return query_to_return
