6807353e9e6136ab4bdcd7dd951777337bdf3efb
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-23 17:57:44 +0300

    first

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def main() -> QuerySet:
     Genre.objects.create(name="Dramma")
 
     Actor.objects.create(first_name="George", last_name="Clooney")
-
     Actor.objects.create(first_name="Kianu", last_name="Reaves")
     Actor.objects.create(first_name="Will", last_name="Smith")
     Actor.objects.create(first_name="Jaden", last_name="Smith")
@@ -27,7 +26,7 @@ def main() -> QuerySet:
 
     actor = Actor.objects.get(first_name="Kianu", last_name="Reaves")
     actor.first_name = "Keanu"
-    actor.second_name = "Reeves"
+    actor.last_name = "Reeves"
     actor.save()
 
     Genre.objects.filter(name="Action").delete()
