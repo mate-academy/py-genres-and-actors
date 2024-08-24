2252aae0ede6ccba1b259ee9a9d30b21c294b1ec
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2024-08-24 13:59:21 +0300

    Fixed Flake8

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-def main():
+def main() -> None:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     try:
         from django.core.management import execute_from_command_line
