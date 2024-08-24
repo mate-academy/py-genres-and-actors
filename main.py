7801fff04922fad9655f742a1b0a4ce844bbbf7f
Anton Bilous <anton.bilosus@gmail.com>
2024-08-24 15:10:03 +0300

    Split main into smaller functions

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ ACTORS = [
 ]
 
 
-def main() -> QuerySet:
+def populate_db() -> None:
     for genre in GENRES:
         Genre.objects.create(name=genre)
 
     for first_name, last_name in ACTORS:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
+
+def make_updates() -> None:
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
         first_name="George",
@@ -37,7 +39,17 @@ def main() -> QuerySet:
         last_name="Reaves",
     ).update(first_name="Keanu", last_name="Reeves")
 
+
+def make_deletions() -> None:
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
+
+
+def main() -> QuerySet:
+    populate_db()
+
+    make_updates()
+
+    make_deletions()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
