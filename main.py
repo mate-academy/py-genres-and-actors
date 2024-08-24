040580b5a71b7951e19fd73b276a7142b357e474
Alexandr Politov <apolitov83@gmail.com>
2024-08-24 13:11:45 +0300

    Update main.py
    "dont know how to make outer funcs with filter"

--- main.py
+++ main.py
@@ -1,43 +1,36 @@
 import init_django_orm  # noqa: F401
 
-from django.db import models
-
+from django.db.models import QuerySet
 from db.models import Actor, Genre
 
-def create_obj(owner: type(models), **kwargs):
-    owner.objects.create(**kwargs)
 
-def main() -> models.QuerySet:
+def main() -> QuerySet:
 
     new_genres = ["Western", "Action", "Dramma"]
     for genre in new_genres:
-        create_obj(owner=Genre, name=genre)
+        Genre.objects.create(name=genre)
 
-    new_actors = [
-        ("George", "Klooney"),
-        ("Kianu", "Reaves"),
-        ("Scarlett", "Keegan"),
-        ("Will", "Smith"),
-        ("Jaden", "Smith"),
-        ("Scarlett", "Johansson")
-    ]
-    for first, last in new_actors:
-        create_obj(owner=Actor, first_name=first, last_name=last)
+    new_actors = ["George Klooney", "Kianu Reaves",
+                  "Scarlett Keegan", "Will Smith",
+                  "Jaden Smith", "Scarlett Johansson"]
+    for actor in new_actors:
+        first, last = actor.split(" ")
+        Actor.objects.create(first_name=first, last_name=last)
 
-    Genre.objects.filter(name="Dramma").update(name="Drama")
+    list_of_genres_updates = [("Dramma", "Drama")]
+    for old_genre, new_genre in list_of_genres_updates:
+        Genre.objects.filter(name=old_genre).update(name=new_genre)
 
-    list_of_actors_updates = [
-        [("George", "Klooney"), ("George", "Clooney")],
-        [("Kianu", "Reaves"), ("Keanu", "Reeves")]
-    ]
+    list_of_actors_updates = [("George Klooney", "George Clooney"),
+                              ("Kianu Reaves", "Keanu Reeves")]
     for old_actor, new_actor in list_of_actors_updates:
-        old_first, old_last = old_actor
-        new_first, new_last = new_actor
+        old_first, old_last = old_actor.split(" ")
+        new_first, new_last = new_actor.split(" ")
         Actor.objects.filter(
             first_name=old_first, last_name=old_last
         ).update(first_name=new_first, last_name=new_last)
 
-    Genre.objects.filter(name="Action").delete()
+    Genre.objects.filter(name="Drama").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
     set_of_actors = Actor.objects.filter(
