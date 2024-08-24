227a3939a7f7b5061bb98f4222a3ede8dfb39510
maksym_korniev <maksymkorniev88@gmail.com>
2024-08-24 12:34:43 +0300

    refactored actors creation and created update/retrieve/delete methods

--- main.py
+++ main.py
@@ -1,56 +1,34 @@
+from django.db.models import QuerySet
+
 import init_django_orm  # noqa: F401
 
-from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
     create_genres(("Western", "Action", "Dramma"))
-    create_actors({
-        "actor1": {
-            "first_name": "George",
-            "last_name": "Klooney"
-        },
+    create_actors([("George", "Klooney"), ("Kianu", "Reaves"),
+                   ("Scarlett", "Keegan"), ("Will", "Smith"),
+                   ("Jaden", "Smith"), ("Scarlett", "Johansson")])
 
-        "actor2": {
-            "first_name": "Kianu",
-            "last_name": "Reaves"
-        },
-
-        "actress1": {
-            "first_name": "Scarlett",
-            "last_name": "Keegan"
-        },
-
-        "actor3": {
-            "first_name": "Will",
-            "last_name": "Smith"
-        },
-
-        "actor4": {
-            "first_name": "Jaden",
-            "last_name": "Smith"
-        },
-
-        "actress2": {
-            "first_name": "Scarlett",
-            "last_name": "Johansson"
-        },
-    })
-
-    # update
-    Genre.objects.filter(name="Dramma").update(name="Drama")
-
-    (Actor.objects.filter(first_name="George", last_name="Klooney")
-     .update(last_name="Clooney"))
-    (Actor.objects.filter(first_name="Kianu", last_name="Reaves")
-     .update(first_name="Keanu", last_name="Reeves"))
+    update_genre("Dramma", "Drama")
+    update_actor(
+        first_name_to_find="George",
+        last_name_to_find="Klooney",
+        last_name_to_update="Clooney"
+    )
+    update_actor(
+        first_name_to_find="Kianu",
+        last_name_to_find="Reaves",
+        first_name_to_update="Keanu",
+        last_name_to_update="Reeves"
+    )
 
     # delete
-    Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(first_name="Scarlett").delete()
+    delete_genre("Action")
+    delete_actor(first_name="Scarlett")
 
-    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+    return actor_order("first_name", last_name="Smith")
 
 
 def create_genres(genres_names: tuple) -> None:
@@ -58,7 +36,48 @@ def create_genres(genres_names: tuple) -> None:
         Genre.objects.create(name=name)
 
 
-def create_actors(actors: dict) -> None:
-    for actor in actors.values():
-        Actor.objects.create(first_name=actor["first_name"],
-                             last_name=actor["last_name"])
+def create_actors(actors: list[tuple[str, str]]) -> None:
+    for actor in actors:
+        first_name, last_name = actor
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
+
+def update_genre(name_to_find: str, name_to_update: str) -> None:
+    Genre.objects.filter(name=name_to_find).update(name=name_to_update)
+
+
+def update_actor(
+        first_name_to_find: str,
+        last_name_to_find: str = None,
+        first_name_to_update: str = None,
+        last_name_to_update: str = None
+) -> None:
+    actors = Actor.objects.filter(first_name=first_name_to_find)
+    if last_name_to_find is not None:
+        actors = actors.filter(last_name=last_name_to_find)
+
+    if last_name_to_update and first_name_to_update:
+        actors.update(
+            first_name=first_name_to_update,
+            last_name=last_name_to_update
+        )
+    elif last_name_to_find is None:
+        actors.update(first_name=first_name_to_find)
+    else:
+        actors.update(last_name=last_name_to_update)
+
+
+def delete_genre(name_to_delete: str) -> None:
+    Genre.objects.filter(name=name_to_delete).delete()
+
+
+def delete_actor(**names_to_delete) -> None:
+    Actor.objects.filter(**names_to_delete).delete()
+
+
+def actor_order(*order_by, **filter_kwargs) -> QuerySet:
+    return Actor.objects.filter(**filter_kwargs).order_by(*order_by)
+
+
+if __name__ == "__main__":
+    print(main())
