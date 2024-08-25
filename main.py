0c161b8b45ec1903d78a9ba7ad1d4da36f677afb
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-25 12:13:35 +0300

    first

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+
+    Actor.objects.create(first_name="George", last_name="Clooney")
+    Actor.objects.create(first_name="Keanu", last_name="Reeves")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    genre = Genre.objects.get(name="Dramma")
+    genre.name = "Drama"
+    genre.save()
+
+    actor = Actor.objects.get(first_name="George", last_name="Clooney")
+    actor.last_name = "Clooney"
+    actor.save()
+
+    actor = Actor.objects.get(first_name="Keanu", last_name="Reeves")
+    actor.first_name = "Keanu"
+    actor.last_name = "Reeves"
+    actor.save()
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    new_query = Actor.objects.filter(last_name="Smith").order_by("first_name")
+    return new_query
