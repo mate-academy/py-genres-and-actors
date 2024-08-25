654632d3360e6117f390daf6ab861837420207e6
olena4reunited <olenaduringintermission@gmail.com>
2024-08-25 19:18:22 +0300

    fix: simplified functions, unpacked tuples instead of indexing

--- main.py
+++ main.py
@@ -5,77 +5,66 @@ from django.db.models import QuerySet
 from db.models import Actor, Genre
 
 
-def create_genre(data: list[str]) -> None:
+def create(data: list[str] | list[tuple[str, str]]) -> None:
     if isinstance(data[0], str):
         for row in data:
             Genre.objects.create(name=row)
-
-
-def create_actor(data: list[tuple[str, str]]) -> None:
     if isinstance(data[0], tuple):
         for row in data:
-            Actor.objects.create(first_name=row[0], last_name=row[1])
+            first_name, last_name = row
+            Actor.objects.create(first_name=first_name, last_name=last_name)
 
 
-def update_genre(
-        data: str | list[str],
-        new_data: str | list[str]
+def _update_genre(
+        name: str,
+        new_name: str
+) -> None:
+    Genre.objects.filter(name=name).update(name=new_name)
+
+
+def _update_actor(
+        fl_name: tuple[str, str],
+        new_fl_name: tuple[str, str]
+) -> None:
+    first_name, last_name = fl_name
+    new_first_name, new_last_name = new_fl_name
+    Actor.objects.filter(
+        first_name=first_name,
+        last_name=last_name
+    ).update(
+        first_name=new_first_name,
+        last_name=new_last_name
+    )
+
+
+def update(
+        data: list[str | tuple[str, str]] | str | tuple[str, str],
+        new_data: list[str | tuple[str, str]] | str | tuple[str, str]
 ) -> None:
     if isinstance(data, str) and isinstance(new_data, str):
-        Genre.objects.filter(name=data).update(name=new_data)
+        _update_genre(data, new_data)
+    if isinstance(data, tuple) and isinstance(new_data, tuple):
+        _update_actor(data, new_data)
     if isinstance(data, list) and isinstance(new_data, list):
         if isinstance(data[0], str):
             for i in range(len(data)):
-                Genre.object.filter(name=data[i]).update(name=new_data[i])
+                _update_genre(data[i], new_data[i])
+        if isinstance(data[0], tuple):
+            for i in range(len(data)):
+                _update_actor(data[i], new_data[i])
 
 
-def update_actor(
-        data: tuple[str, str] | list[tuple[str, str]],
-        new_data: tuple[str, str] | list[tuple[str, str]],
+def delete(
+        genre_name: str | None = None,
+        actor_first_name: str | None = None,
+        actor_last_name: str | None = None,
 ) -> None:
-    if isinstance(data, tuple) and isinstance(new_data, tuple):
-        Actor.objects.filter(
-            first_name=data[0],
-            last_name=data[1]
-        ).update(
-            first_name=new_data[0],
-            last_name=new_data[1]
-        )
-    if isinstance(data, list) and isinstance(new_data, list):
-        for data, new_data in zip(data, new_data):
-            Actor.objects.filter(
-                first_name=data[0],
-                last_name=data[1]
-            ).update(
-                first_name=new_data[0],
-                last_name=new_data[1]
-            )
-
-
-def delete_genre(
-        name: str | list[str]
-) -> None:
-    if isinstance(name, str):
-        Genre.objects.filter(name=name).delete()
-    if isinstance(name, list):
-        for row in name:
-            Genre.objects.filter(name=row).delete()
-
-
-def delete_actor(
-        first_name: str | None = None,
-        last_name: str | None = None
-) -> None:
-    if isinstance(first_name, str) and isinstance(last_name, str):
-        Actor.objects.filter(
-            first_name=first_name,
-            last_name=last_name
-        ).delete()
-    elif isinstance(first_name, str) and last_name is None:
-        Actor.objects.filter(first_name=first_name).delete()
-
-    elif first_name is None and isinstance(last_name, str):
-        Actor.objects.filter(last_name=last_name).delete()
+    if genre_name is not None:
+        Genre.objects.filter(name=genre_name).delete()
+    if actor_first_name is not None:
+        Actor.objects.filter(first_name=actor_first_name).delete()
+    if actor_last_name is not None:
+        Actor.objects.filter(last_name=actor_last_name).delete()
 
 
 def main() -> QuerySet:
@@ -90,16 +79,16 @@ def main() -> QuerySet:
         ("Scarlett", "Johansson")
     ]
 
-    create_genre(genres)
-    create_actor(actors)
+    create(genres)
+    create(actors)
 
-    update_genre("Dramma", "Drama")
-    update_actor(
+    update("Dramma", "Drama")
+    update(
         [("George", "Klooney"), ("Kianu", "Reaves")],
         [("George", "Clooney"), ("Keanu", "Reeves")]
     )
 
-    delete_genre("Action")
-    delete_actor(first_name="Scarlett")
+    delete(genre_name="Action")
+    delete(actor_first_name="Scarlett")
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
