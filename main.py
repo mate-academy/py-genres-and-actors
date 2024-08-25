8ff8c5053a922ad230ca613665ae26e731eac86a
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-25 11:20:08 +0300

    implement(fixed)

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def main() -> QuerySet:
     Genres.objects.filter(name="Action").delete()
     Actors.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+    return Actors.objects.filter(last_name="Smith").order_by("first_name")
