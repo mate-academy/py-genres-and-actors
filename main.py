88736d69e53c4696ef2bfc1abb95084a3f6f22e9
Alexandr Politov <apolitov83@gmail.com>
2024-08-26 09:37:27 +0300

    Solution Politov
    added checklist.md

--- main.py
+++ main.py
@@ -5,15 +5,10 @@ from django.db import models
 from db.models import Actor, Genre
 
 
-def create_obj(owner: type(models), **kwargs) -> None:
-    owner.objects.create(**kwargs)
-
-
-def main() -> models.QuerySet:
-
+def create() -> None:
     new_genres = ["Western", "Action", "Dramma"]
     for genre in new_genres:
-        create_obj(owner=Genre, name=genre)
+        Genre.objects.create(name=genre)
 
     new_actors = [
         ("George", "Klooney"),
@@ -24,8 +19,10 @@ def main() -> models.QuerySet:
         ("Scarlett", "Johansson")
     ]
     for first, last in new_actors:
-        create_obj(owner=Actor, first_name=first, last_name=last)
+        Actor.objects.create(first_name=first, last_name=last)
 
+
+def update() -> None:
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
     list_of_actors_updates = [
@@ -39,8 +36,19 @@ def main() -> models.QuerySet:
             first_name=old_first, last_name=old_last
         ).update(first_name=new_first, last_name=new_last)
 
+
+def delete() -> None:
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
+
+
+def main() -> models.QuerySet:
+
+    create()
+
+    update()
+
+    delete()
 
     set_of_actors = Actor.objects.filter(
         last_name="Smith").order_by("first_name")
