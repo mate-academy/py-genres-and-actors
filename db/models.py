e54d36ab9a5e3289c2dbe0b35bfb727c79a0837d
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-26 17:20:18 +0300

    implement(try)

--- db/models.py
+++ db/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class Genres(models.Model):
+class Genre(models.Model):
     name = models.CharField(max_length=255)
 
 
-class Actors(models.Model):
+class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
