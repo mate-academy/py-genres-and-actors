e8836d3dd374fac5fbef5ef0643182d0f1183be7
FalseCoreCentury <simonov0101k@gmail.com>
2024-09-11 15:28:57 +0200

    Solution

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import init_django_orm  # noqa: F401
+
 from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    genres = ["Western", "Action", "Dramma"]
+    genre_list = ["Western", "Action", "Dramma"]
 
-    for genre in genres:
+    for genre in genre_list:
         Genre.objects.create(name=genre)
 
-    actors = [
+    actor_list = [
         ("George", "Klooney"),
         ("Kianu", "Reaves"),
         ("Scarlett", "Keegan"),
@@ -17,22 +19,20 @@ def main() -> QuerySet:
         ("Scarlett", "Johansson")
     ]
 
-    for first_name, last_name in actors:
+    for first_name, last_name in actor_list:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(
-        first_name="George", last_name="Clooney"
-    )
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
-        first_name="Keanu", last_name="Reeves"
-    )
+    Actor.objects.filter(first_name="George").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu").update(
+        first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    query_set = Actor.objects.filter(last_name="Smith").order_by("first_name")
-    return query_set
+    the_query_set = Actor.objects.filter(
+        last_name="Smith").order_by("first_name")
+    return the_query_set
 
 
 if __name__ == "__main__":
