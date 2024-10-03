54ab13ace5a44a8c9b7d858066c2dbd78a62d99d
Igor Kolisnyk <159931109+Ja0ni@users.noreply.github.com>
2024-10-03 12:17:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ def main() -> QuerySet:
     for name in names:
         Genre.objects.create(name=name)
 
-    actors = [("George", "Klooney"), ("Kianu", "Reaves"),
-              ("Scarlett", "Keegan"), ("Will", "Smith"),
-              ("Jaden", "Smith"), ("Scarlett", "Johansson")]
+    actors = [("George", "Klooney"),
+              ("Kianu", "Reaves"),
+              ("Scarlett", "Keegan"),
+              ("Will", "Smith"),
+              ("Jaden", "Smith"),
+              ("Scarlett", "Johansson")]
     for first_name, last_name in actors:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney"
-                         ).update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves"
-                         ).update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
