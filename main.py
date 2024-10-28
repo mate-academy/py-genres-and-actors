ff57fb7703fe8f235103975a51739b09b05e07b2
Dima Havrylenko <94nj111@gmail.com>
2024-10-28 22:12:20 +0200

    genres-and-actors (flake fix)

--- main.py
+++ main.py
@@ -19,17 +19,8 @@ def main() -> QuerySet:
         Actor.objects.create(first_name=first_name, last_name=last_name)
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu", last_name="Reeves")
+    (Actor.objects.filter(first_name="Kianu")
+     .update(first_name="Keanu", last_name="Reeves"))
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
     return Actor.objects.filter(last_name="Smith").all().order_by("first_name")
-
-
-print(main())
-# <QuerySet [<Actor: Jaden Smith>, <Actor: Will Smith>]>
-
-print(Genre.objects.all())
-# <QuerySet [<Genre: Western>, <Genre: Drama>]>
-
-print(Actor.objects.all())
-# <QuerySet [<Actor: George Clooney>, <Actor: Keanu Reeves>, <Actor: Will Smith>, <Actor: Jaden Smith>]>
