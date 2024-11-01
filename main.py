05970cbed96590df9091145378f42bc1e3b28c39
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-11-01 22:27:48 +0200

    'solution'

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import init_django_orm  # noqa: F401
 from db.models import Genre, Actor
 
 
-def create():
+def create() -> None:
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
 
 
-def create_actors():
+def create_actors() -> None:
     Actor.objects.create(
         first_name="George",
         last_name="Klooney",)
@@ -34,18 +34,20 @@ def create_actors():
         first_name="Scarlett",
         last_name="Johansson", )
 
-def update_actors_and_ganre():
+def update_actors_and_ganre() -> None:
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu",
+                                                    last_name="Reeves")
 
 
-def delete_actors_and_ganres():
+def delete_actors_and_ganres() -> None:
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-def return_query_set():
+def return_query_set() -> None:
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
 def main():
     create()
     create_actors()
@@ -53,5 +55,3 @@ def main():
     delete_actors_and_ganres()
     return_query_set()
     return return_query_set()
-
-
