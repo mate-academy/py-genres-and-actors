3b02eb7d55ebef3592390b328b6e7edd3fdcce38
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-11-01 11:04:56 +0200

    Solution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from venv import create
+# from venv import create
 
 import init_django_orm  # noqa: F401
 
@@ -19,8 +19,12 @@ def main() -> QuerySet:
         a_name, surname = i.split(" ")
         Actor.objects.create(first_name=a_name, last_name=surname)
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney"
+    ).update(last_name="Clooney")
+    (Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves"))
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
     print(Actor.objects.filter(last_name="Smith").order_by("first_name").all())
