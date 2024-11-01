69c46800ac177109f71ed89e983bb17b9fbda6fb
Bondzik-S <bondziks@gmail.com>
2024-11-01 15:07:03 +0200

    Solution

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ from db.models import Genre, Actor
 def main() -> QuerySet:
     genres=["Western", "Action", "Dramma"]
 
-    actors_name = ["George", "Kianu", "Scarlett",
-                    "Will", "Jaden", "Scarlett"]
-    actors_surname = ["Klooney", "Reaves", "Keegan",
-                       "Smith", "Smith", "Johansson"]
+    actors_name = ["George", "Kianu", "Scarlett", "Will", "Jaden", "Scarlett"]
+    actors_surname = ["Klooney", "Reaves", "Keegan", "Smith", "Smith", "Johansson"]
 
     for genre in genres:
         Genre.objects.create(
