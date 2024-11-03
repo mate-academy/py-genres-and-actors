63dda949f1541ba418a906c0b20f10afdb21eae9
Matvii Safonov <matveysafonov2@gmail.com>
2024-11-03 15:45:34 +0200

    Solution

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    genres = [
+        ("Western",),
+        ("Action",),
+        ("Dramma",)
+    ]
+    for name, in genres:
+        Genre.objects.create(name=name)
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
@@ -30,13 +38,8 @@ def main() -> QuerySet:
     Genre.objects.filter(name="Action").delete()
 
     Actor.objects.filter(first_name="Scarlett").delete()
+
     smith_actors = Actor.objects.filter(
         last_name="Smith"
     ).order_by("first_name")
     return smith_actors
-
-
-if __name__ == "__main__":
-    smith_actors = main()
-    for actor in smith_actors:
-        print(actor)
