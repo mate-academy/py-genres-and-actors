dfa1d9cd1265aa5eebc01dc8e206ce12d30a46ed
Alex <delete.rap.stupid@gmail.com>
2024-11-03 19:02:11 +0100

    solution

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from venv import create
-
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
@@ -7,8 +5,16 @@ from django.db.models import QuerySet
 
 from db.models import Actor, Genre
 
+
 def main() -> QuerySet:
-    actors = [("George", "Klooney"), ("Kianu", "Reaves"), ("Scarlett", "Keegan"), ("Will", "Smith"), ("Jaden", "Smith"), ("Scarlett", "Johansson")]
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
     genres = ["Western", "Action", "Dramma"]
     for actor_name, actor_surname in actors:
         Actor.objects.create(first_name=actor_name, last_name=actor_surname)
@@ -16,7 +22,11 @@ def main() -> QuerySet:
         Genre.objects.create(name=genre)
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="Kianu"
+    ).update(
+        first_name="Keanu", last_name="Reeves"
+    )
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
