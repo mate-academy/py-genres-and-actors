c2b1ec3ed088bc64a76ead9a7ac0c37cd607abd1
Ivan Fursyk <ivan.fursyk@gmail.com>
2024-11-04 19:35:19 +0200

    Solution

--- settings.py
+++ settings.py
@@ -6,6 +6,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
