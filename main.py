2334eb7fd34c9717d5f34279b2b5576748d07ca9
Volodymyr Kovalenko <380974261314kv@gmail.com>
2024-11-05 22:13:00 +0200

    create main function

--- main.py
+++ main.py
@@ -4,17 +4,26 @@ from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
+genres = ["Western", "Action", "Dramma"]
+actors = [
+    ("George Klooney"),
+    ("Kianu Reaves"),
+    ("Scarlett Keegan"),
+    ("Will Smith"),
+    ("Jaden Smith"),
+    ("Scarlett Johansson"),
+]
+
+
 def main() -> QuerySet:
     """Function realeses all CRUD operations"""
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    for genre in genres:
+        Genre.objects.create(name=genre)
+
+    for actor in actors:
+        first_name, last_name = actor.split(" ")
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(first_name="Kianu").update(
