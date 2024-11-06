9dc9c89f256a8e5a3060df32bffa0abfdd4beb92
Illia <ilyachernets@gmail.com>
2024-11-06 15:52:50 +0200

    fix

--- db/models.py
+++ db/models.py
@@ -7,4 +7,4 @@ class Genre(models.Model):
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=255)
