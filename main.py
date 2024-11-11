fa509724f18c17f64335a8f707e87dea72e608ea
ABC <matweyes@gmail.com>
2024-11-11 07:05:47 +0200

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-import init_django_orm  # noqa: F401
-
-from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
-def main() -> QuerySet:
-    pass
+def main() -> None:
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney"
+    ).update(
+        last_name="Clooney"
+    )
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(
+        first_name="Keanu", last_name="Reeves"
+    )
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    smith_actors = Actor.objects.filter(
+        last_name="Smith"
+    ).order_by(
+        "first_name"
+    )
+
+    print("Genres:", Genre.objects.all())
+    print("Actors:", Actor.objects.all())
+
+    return smith_actors
+
+
+if __name__ == "__main__":
+    main()
