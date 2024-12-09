1998e347e4828d8578c55498bc521cc2b842de36
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-12-09 17:48:03 +0100

    solution2

--- main.py
+++ main.py
@@ -8,17 +8,22 @@ from db.models import Genre
 
 def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
-    actors = ["George Klooney", "Kianu Reaves", "Scarlett Keegan", "Will Smith", "Jaden Smith", "Scarlett Johansson"]
+    actors = ["George Klooney", "Kianu Reaves",
+              "Scarlett Keegan", "Will Smith",
+              "Jaden Smith", "Scarlett Johansson"]
     for genre in genres:
         Genre.objects.create(name=genre)
 
     for actor in actors:
         first_name, last_name = actor.split(" ")
-        Actor.objects.create(first_name=first_name, last_name=last_name)
+        Actor.objects.create(first_name=first_name,
+                             last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu", last_name="Reeves")
+    (Actor.objects.filter(last_name="Klooney").
+     update(last_name="Clooney"))
+    (Actor.objects.filter(first_name="Kianu").
+     update(first_name="Keanu", last_name="Reeves"))
 
     action_genre = Genre.objects.filter(name="Action")
     if action_genre is not None:
