80745fc70b18a8035443d524aad0f0b6c74f645d
Ilia Terentev <wenhun1@gmail.com>
2024-12-24 13:21:25 +0200

    Try 3

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Kianu", last_name="Reaves")
     Actor.objects.create(first_name="Scarlett", last_name="Keegan")
     Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="George", last_name="Klooney")
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
