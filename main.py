7d82169f41a8be3c450bde91a2567a13f82fb52b
Artem Ig <artem.ig1990@gmail.com>
2025-01-03 16:24:08 +0200

    solution1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    gernes_list = ['Western', 'Action', 'Dramma']
+    gernes_list = ["Western", "Action", "Dramma"]
     for gerne in gernes_list:
         Genre.objects.create(name=gerne)
 
@@ -16,19 +16,19 @@ def main() -> QuerySet:
         Actor.objects.create(first_name=actor[0],
                              last_name=actor[1])
 
-    Genre.objects.filter(name='Dramma').update(
-        name='Drama'
+    Genre.objects.filter(name="Dramma").update(
+        name="Drama"
     )
-    Actor.objects.filter(first_name='George').update(
-        last_name='Clooney'
+    Actor.objects.filter(first_name="George").update(
+        last_name="Clooney"
     )
-    Actor.objects.filter(first_name='Kianu').update(
+    Actor.objects.filter(first_name="Kianu").update(
         first_name='Keanu',
         last_name='Reeves'
     )
-    Genre.objects.filter(name='Action').delete()
-    Actor.objects.filter(first_name='Scarlett').delete()
-    print(Actor.objects.filter(last_name='Smith').order_by(
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+    print(Actor.objects.filter(last_name="Smith").order_by(
         "first_name"
     ))
 
