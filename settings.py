6f160335cd93375db31257785b233fc410389a06
Maksym <chukhno.maksym@gmail.com>
2025-01-06 22:42:26 +0200

    Solution

--- settings.py
+++ settings.py
@@ -8,6 +8,10 @@ SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
+INSTALLED_APPS = [
+    "db"
+]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
