7220da438833a60d3eaa7a890b6b1ef4860c7b95
Dmytro <dima.sichkar2003@gmail.com>
2025-01-07 17:24:30 +0200

    solution

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main() -> QuerySet:
     Actor.objects.filter(first_name="George",
                          last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(first_name="Kianu",
-                         last_name="Reaves").update(first_name="Keeanu",
+                         last_name="Reaves").update(first_name="Keanu",
                                                     last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
