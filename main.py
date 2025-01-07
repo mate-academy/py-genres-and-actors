9c9945ef5e681bb02a5bcc238bd464fa05390969
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-01-07 17:11:59 +0200

    genre-actor_solution

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ from db.models import Genre, Actor
 def main() -> QuerySet:
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Drama")
-    Genre.objects.create(name="Drama")
+    Genre.objects.create(name="Comedy")
+
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Will", last_name="Smith")
     Actor.objects.create(first_name="George", last_name="Clooney")
     Actor.objects.create(first_name="Keanu", last_name="Reeves")
 
-    Genre.object.filter(name="Drama").update(name="Drama")
-    Actor.objects.filter(first_name="Jaden",
-                         last_name="Smith").update(first_name="Jaden",last_name="Smith")
-    Genre.objects.filter(name="Drama").delete()
-    Actor.objects.filter(first_name="Jaden").delete()
-    return Actor.objects.filter(first_name="Jaden").order_by("last_name")
+    Genre.objects.filter(name="Drama").update(name="Drama Updated")
+
+    Actor.objects.filter(first_name="Jaden", last_name="Smith").update(
+        first_name="Jaden Updated", last_name="Smith Updated"
+    )
+
+    Genre.objects.filter(name="Drama Updated").delete()
+
+    Actor.objects.filter(first_name="Jaden Updated").delete()
+
+    return Actor.objects.order_by("last_name")
