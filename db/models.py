866ad6835a951936fd87a42dca043f4bb865d559
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-01-07 17:05:55 +0200

    genre-actor_solution

--- db/models.py
+++ db/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-
 class Genre(models.Model):
     name = models.CharField(max_length=100)
+
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
--- main.py
+++ main.py
@@ -13,13 +13,10 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="George", last_name="Clooney")
     Actor.objects.create(first_name="Keanu", last_name="Reeves")
 
-    Genre.object.filter(name="Drama").update(name = "Drama")
+    Genre.object.filter(name="Drama").update(name="Drama")
     Actor.objects.filter(first_name="Jaden",
-                         last_name = "Smith").update(first_name = "Jaden",
-                                                     last_name = "Smith")
+                         last_name="Smith").update(first_name="Jaden",
+                                                     last_name="Smith")
     Genre.objects.filter(name="Drama").delete()
     Actor.objects.filter(first_name="Jaden").delete()
     return Actor.objects.filter(first_name="Jaden").order_by("last_name")
-
-
-
