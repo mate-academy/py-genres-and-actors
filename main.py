804af1d94b7d3550530060279c41af5e6f59b6e9
Volodymyr Havrylenko <vladimirvgavrilenko@gmail.com>
2025-01-15 00:51:30 +0200

    Added two tables, migrated and seeded data

--- main.py
+++ main.py
@@ -2,6 +2,32 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Actor
+from db.models import Genre
+
 
 def main() -> QuerySet:
-    pass
+    genres = ("Western", "Action", "Dramma")
+    actors = (
+        {"row_first_name" : "Goerge", "row_last_name" : "Klooney"},
+        {"row_first_name" : "Kianu", "row_last_name" : "Reaves" },
+        {"row_first_name" : "Scarlett", "row_last_name" : "Keegan" },
+        {"row_first_name" : "Will", "row_last_name" : "Smith"},
+        {"row_first_name" : "Jaden", "row_last_name" : "Smith" },
+        {"row_first_name" : "Scarlett", "row_last_name" : "Johansson" },
+    )
+    # for genre in genres:
+    #     Genre.objects.create(
+    #     name = genre
+    # )
+    # for actor in actors:
+    #     Actor.objects.create(
+    #         first_name = actor["row_first_name"],
+    #         last_name = actor["row_last_name"]
+    #     )
+
+    print(Genre.objects.all())
+    print(Actor.objects.all())
+
+
+main()
