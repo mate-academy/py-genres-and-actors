d682261a3e61d66714a22d93c33b195c2ba63e5f
unknown <maximuschampion2002@gmail.com>
2025-01-20 16:12:52 +0200

    genres-and-actors

--- db/models.py
+++ db/models.py
@@ -2,17 +2,9 @@ from django.db import models
 
 
 class Genre(models.Model):
-<<<<<<< HEAD
     name = models.CharField(max_length=255)
-=======
-    genre_name = models.CharField(max_length=255)
->>>>>>> b2582479ebab56ab307457b545a068a061971d82
 
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
-<<<<<<< HEAD
     last_name = models.CharField(max_length=255)
-=======
-    last_name = models.CharField(max_length=255)
->>>>>>> b2582479ebab56ab307457b545a068a061971d82
