ba8e3623b7e617c9bba9cbf858dbb3a906782feb
bymik <bymikola@gmail.com>
2025-01-28 22:09:58 +0200

    1
    Signed-off-by: bymik <bymikola@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import init_django_orm  # noqa: F401
-
 from django.db.models import QuerySet
+import init_django_orm  # noqa: F401
 from db.models import Actor, Genre
-
 
 
 def main() -> QuerySet:
@@ -17,33 +15,34 @@ def main() -> QuerySet:
         ("Will", "Smith"),
         ("Jaden", "Smith"),
         ("Scarlett", "Johansson"),
-        ]
+    ]
 
     # Create
     for data in genre_to_create:
         Genre.objects.create(name=data, )
 
     for data_name, data_last_name in actor_to_create:
-        print(data_name, data_last_name)
         Actor.objects.create(
             first_name=data_name,
             last_name=data_last_name
         )
 
-    #Update
+    # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name = "Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name= "Keanu", last_name= "Reeves")
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney"
+    ).update(last_name="Clooney")
 
-    #DELETE
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+
+    # DELETE
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
-
-
-
-
-
-
+print(Actor.objects.all())
