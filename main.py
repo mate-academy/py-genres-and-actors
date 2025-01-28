19026b34d4b14205296f0a021d989dab978773ed
Dell <gyrkalomas@gmail.com>
2025-01-28 13:03:10 +0300

    fix return

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main() -> QuerySet:
         first_name="Scarlett"
     ).delete()
 
-    print(Actor.objects.filter(last_name="Smith").order_by("first_name"))
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
 if __name__ == "__main__":
