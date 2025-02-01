f0a815e109c0886080385f66c0c99f2f9f3f50ac
shafran <shafran.andriy@gmail.com>
2025-02-01 20:15:55 +0200

    Created all queries

--- db/models.py
+++ db/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
-    def __str__(self):
-        return self.name
 
 
 class Actor(models.Model):
