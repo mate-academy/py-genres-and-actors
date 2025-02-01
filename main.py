6a8a1a5f5422229ba5384c4a3777e676230e0389
shafran <shafran.andriy@gmail.com>
2025-02-01 20:12:40 +0200

    Created all queries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from django.template.defaultfilters import first
+from django.template.defaultfilters import first  # noqa: F401
 
 import init_django_orm  # noqa: F401
 
@@ -7,35 +7,33 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(
-        name="Western",)
-    Genre.objects.create(
-        name="Action", )
-    Genre.objects.create(
-        name="Dramma", )
-    Actor.objects.create(
-        first_name="George", last_name="Klooney")
-    Actor.objects.create(
-        first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(
-        first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(
-        first_name="Will", last_name="Smith")
-    Actor.objects.create(
-        first_name="Jaden", last_name="Smith")
-    Actor.objects.create(
-        first_name="Scarlett", last_name="Johansson")
-    Genre.objects.filter(name="Dramma",
-                                          ).update(format="Drama")
-    Actor.objects.filter(last_name="Klooney",
-                             ).update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves"
-                             ).update(first_name="Keanu", last_name="Reeves",)
-    Genre.objects.filter(name="Action",
-                       ).delete()
-    Actor.objects.filter(first_name="Scarlett",
-                        ).delete()
-    Genre.objects.filter(last_name="Smith").order_by(Actor.first_name)
+    Genre.objects.create(name="Western",)
+    Genre.objects.create(name="Action", )
+    Genre.objects.create(name="Dramma", )
+    Actor.objects.create(first_name="George",
+                         last_name="Klooney")
+    Actor.objects.create(first_name="Kianu",
+                         last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett",
+                         last_name="Keegan")
+    Actor.objects.create(first_name="Will",
+                         last_name="Smith")
+    Actor.objects.create(first_name="Jaden",
+                         last_name="Smith")
+    Actor.objects.create(first_name="Scarlett",
+                         last_name="Johansson")
+    Genre.objects.filter(name="Dramma",).update(format="Drama")
+    (Actor.objects.filter(last_name="Klooney",)
+     .update(last_name="Clooney"))
+    Actor.objects.filter(first_name="Kianu",
+                         last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves",)
+    Genre.objects.filter(name="Action",).delete()
+    Actor.objects.filter(first_name="Scarlett",).delete()
+    (Genre.objects.filter(last_name="Smith")
+     .order_by(Actor.first_name))
+
 
 if __name__ == "__main__":
     main()
