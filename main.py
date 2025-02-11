27f1579f4f19abd9c170d18492528c203fd4f79e
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-02-11 17:53:39 +0200

    Solution

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from tkinter.font import names
-
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
@@ -8,15 +6,23 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    genres = ["Western", "Action", "Dramma"]
+
+    actors_names = [
+        ["George", "Klooney"],
+        ["Kianu", "Reaves"],
+        ["Will", "Smith"],
+        ["Jaden", "Smith"],
+        ["Scarlett", "Johansson"],
+    ]
+
+    for genre in genres:
+        Genre.objects.create(name=genre)
+
+    for actor_first, actor_last in actors_names:
+        Actor.objects.create(first_name=actor_first, last_name=actor_last)
+
+
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
