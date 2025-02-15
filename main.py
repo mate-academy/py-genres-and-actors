7b71fbed1545a523a35bea456329de0cfbbe8184
Yasha Lapin <yasha.lapin@gmail.com>
2025-02-15 14:09:10 +0200

    GenresAndActors2

--- main.py
+++ main.py
@@ -16,13 +16,17 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="George",
+                         last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu",
+                         last_name="Reaves").update(first_name="Keanu",
+                                                    last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    smith_actors = Actor.objects.filter(last_name="Smith").order_by("first_name")
+    smith_actors = (Actor.objects.filter(last_name="Smith").
+                    order_by("first_name"))
     return smith_actors
 
 
