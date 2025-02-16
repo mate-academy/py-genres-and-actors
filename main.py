8b4f0e6f9a9119133539e553ecb09cbd35f8f038
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-02-16 17:13:01 +0100

    'Solution'

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import init_django_orm  # noqa: F401
 from django.db.models import QuerySet
 from db.models import Genre, Actor
 
+
 def main() -> QuerySet:
     genres = [
         Genre(name="Western"),
