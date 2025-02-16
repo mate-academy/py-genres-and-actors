4a95d2d038e8b11903e543f3d6997ac9ae1b6abb
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-02-16 17:09:31 +0100

    'Solution'

--- main.py
+++ main.py
@@ -3,22 +3,18 @@ from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    genres = [
+        Genre(name="Western"),
+        Genre(name="Drama")
+    ]
+    Genre.objects.bulk_create(genres)
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
-
-    Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
-
-    Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(first_name="Scarlett").delete()
+    actors = [
+        Actor(first_name="George", last_name="Clooney"),
+        Actor(first_name="Keanu", last_name="Reeves"),
+        Actor(first_name="Will", last_name="Smith"),
+        Actor(first_name="Jaden", last_name="Smith")
+    ]
+    Actor.objects.bulk_create(actors)
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
