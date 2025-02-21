4d9884bd7a425b440e99fdee860c23230acb9d0f
Marek Smolana <msmolana@gmail.com>
2025-02-21 12:54:49 +0100

    code updated based on ai mentor comments

--- main.py
+++ main.py
@@ -7,24 +7,21 @@ from db.models import Genre, Actor
 def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
     for genre in genres:
-        Genre.objects.create(genre)
-
+        Genre.objects.create(name=genre)
     for actor_name in ["George Klooney",
                        "Kianu Reaves",
                        "Scarlett Keegan",
                        "Will Smith",
                        "Jaden Smith",
                        "Scarlet Johansson"]:
-        Actor.objects.create(actor_name.split(" "))
-
-    Genre.objects.update(name="Dramma", new_name="Drama")
-
-    Actor.objects.update(last_name="Klooney", new_name="Clooney")
-    Actor.objects.update(first_name="Kianu", new_name="Keanu")
-    Actor.objects.update(last_name="Reaves", new_name="Reeves")
-
-    Genre.objects.delete("Action")
-    Actor.objects.delete(first_name="Scarlett")
+        name_parts = actor_name.split(" ")
+        Actor.objects.create(first_name=name_parts[0], last_name=name_parts[1])
+    Genre.objects.filter(name="Dramma").update(new_name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(name="Clooney")
+    Actor.objects.filter(first_name="Kianu").update(name="Keanu")
+    Actor.objects.filter(last_name="Reaves").update(name="Reeves")
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by('last_name')
 
