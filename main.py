8b7a572e7967f6807144c115da32a202a42ebd7d
Alex Plugin <plugin0785@gmail.com>
2025-02-22 18:02:19 +0200

    Solution

--- main.py
+++ main.py
@@ -33,6 +33,5 @@ def main() -> QuerySet:
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
-# if __name__ == "__main__":
-#     print("Calling main()...")
-#     main()
+if __name__ == "__main__":
+    main()
