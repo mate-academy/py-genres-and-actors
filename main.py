d2949fc504b5a037aeb4eaf23b78d81f6f4f07a2
primeglorious <TheComboPunk@gmail.com>
2025-03-15 10:41:45 +0200

    test

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.create(name="Action")
+    print(Genre.objects.all())
+    print(Actor.objects.all())
+
+
+if __name__ == "__main__":
+    main()
