8860283889abdf77e932586016a0de342b158772
Nikita Korenevskyi <korenevsky37@gmail.com>
2025-03-15 18:44:33 +0200

    CommandError: You must set settings.ALLOWED_HOSTS if DEBUG is False. Test: QuerySet.create() takes 1 positional argument but 2 were given

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Actor, Genre
 
 
 def main() -> QuerySet:
-    pass
+    genres = ["Western", "Action", "Dramma"]
+    for genre in genres:
+        Genre.objects.create(genre)
+
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Smith", "Wilson"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name, last_name)
+
+    Genre.objects.filter(
+        name='Dramma'
+    ).update("Drama")
+
+    Actor.objects.filter(
+        last_name='Klooney'
+    ).update("Clooney")
+
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name='Reaves'
+    ).update("Keanu", "Reeves")
+
+    Genre.objects.filter(
+        name='Action'
+    ).delete()
+
+    Genre.objects.filter(
+        name='Scarlett'
+    ).delete()
+
+    return QuerySet(
+        Actor.objects.filter(
+            last_name='Smith'
+        ).order_by('first_name')
+    )
+
+
+if __name__ == '__main__':
+    print(main())
+    # <QuerySet [<Actor: Jaden Smith>, <Actor: Will Smith>]>
+
+    print(Genre.objects.all())
+    # <QuerySet [<Genre: Western>, <Genre: Drama>]>
+
+    print(Actor.objects.all())
