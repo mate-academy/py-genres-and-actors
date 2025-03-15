6c916560b54cac678c8e508f0efbb8c5c54349e0
Nikita Korenevskyi <korenevsky37@gmail.com>
2025-03-15 19:46:17 +0200

    Done

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from django.template.defaultfilters import first
+
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
@@ -7,53 +9,39 @@ from db.models import Actor, Genre
 def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
     for genre in genres:
-        Genre.objects.create(genre)
+        Genre.objects.create(name=genre)
 
     actors = [
         ("George", "Klooney"),
         ("Kianu", "Reaves"),
         ("Scarlett", "Keegan"),
         ("Will", "Smith"),
-        ("Smith", "Wilson"),
         ("Jaden", "Smith"),
         ("Scarlett", "Johansson")
     ]
     for first_name, last_name in actors:
-        Actor.objects.create(first_name, last_name)
+        Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(
         name='Dramma'
-    ).update("Drama")
+    ).update(name="Drama")
 
     Actor.objects.filter(
         last_name='Klooney'
-    ).update("Clooney")
+    ).update(last_name="Clooney")
 
     Actor.objects.filter(
         first_name="Kianu",
         last_name='Reaves'
-    ).update("Keanu", "Reeves")
+    ).update(first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(
         name='Action'
     ).delete()
 
-    Genre.objects.filter(
-        name='Scarlett'
+    Actor.objects.filter(
+        first_name='Scarlett'
     ).delete()
 
-    return QuerySet(
-        Actor.objects.filter(
-            last_name='Smith'
-        ).order_by('first_name')
-    )
-
-
-if __name__ == '__main__':
-    print(main())
-    # <QuerySet [<Actor: Jaden Smith>, <Actor: Will Smith>]>
-
-    print(Genre.objects.all())
-    # <QuerySet [<Genre: Western>, <Genre: Drama>]>
-
-    print(Actor.objects.all())
+    smith_actors = Actor.objects.filter(last_name='Smith').order_by('first_name')
+    return smith_actors
