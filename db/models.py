33e275f0f04aa85bba083e832b57b1f15cd05e0a
PrimeGlorious <TheComboPunk@gmail.com>
2025-03-15 13:35:01 +0200

    Implement Genre and Actor models

--- db/models.py
+++ db/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
-    def __repr__(self):
-        return f"Genre: {self.name}"
+    def __repr__(self) -> str:
+        return (f"Genre: {self.name}, "
+                f"ID: {self.id}")
 
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    def __repr__(self) -> str:
+        return (f"First name: {self.first_name}, "
+                f"Last Name: {self.last_name}, "
+                f"ID: {self.id}")
