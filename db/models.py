9c4eb624ef535ef9d4b27fcc505787f00e888140
Grigoriy Sventsitskiy <g.gesiod@gmail.com>
2025-03-16 23:23:49 +0200

    First Commit

--- db/models.py
+++ db/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-class
+
+class Genre(models.Model):
+    name = models.Field(max_length=255)
+
+
+class Actor(models.Model):
+    first_name = models.Field(max_length=255)
+    last_name = models.Field(max_length=255)
--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+
 
 def main() -> QuerySet:
-    pass
+    genre_western = Genre("Western")
+    genre_action = Genre("Action")
+    genre_dramma = Genre("Dramma")
+
+    actor_george_klooney = Actor(first_name = "George", last_name =  "Klooney")
+    actor_kianu_reaves = Actor(first_name = "Kianu", last_name =  "Reaves")
+    actor_scarlett_keegan = Actor(first_name = "Scarlett", last_name =  "Keegan")
+    actor_will_smith = Actor(first_name = "Will", last_name =  "Smith")
+    actor_jaden_smith = Actor(first_name = "Jaden", last_name =  "Smith")
+    actor_scarlett_johansson = Actor(first_name = "Scarlett", last_name =  "Johansson")
+
+
