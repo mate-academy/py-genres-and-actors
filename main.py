e749ee8d0eac2a2c1a488de9b3d168ec82c9ea38
Mykhailo Safronov <isevenpwnz@gmail.com>
2025-03-19 17:50:06 +0200

    Solution

--- main.py
+++ main.py
@@ -87,7 +87,3 @@ def main() -> QuerySet:
                               )  # Добавляем удаление Scarlett Keegan
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == "__main__":
-    main()
