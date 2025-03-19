62dcfd470e2f5fbad7bb31f13cca2b8bdc96068d
Ngayka <ngayka@gmail.com>
2025-03-19 13:25:05 +0200

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    actors = Actor.objects.filter(
+        last_name="Smith",
+    ).order_by(
+        "first_name",)
+
+    return actors
+
+    genres = Genre.objects.all()
+    return genres
+
+print(main())
+# <QuerySet [<Actor: Jaden Smith>, <Actor: Will Smith>]>
+
+print(Genre.objects.all())
+# <QuerySet [<Genre: Western>, <Genre: Drama>]>
+
+print(Actor.objects.all())
+# <QuerySet [<Actor: George Clooney>, <Actor: Keanu Reeves>, <Actor: Will Smith>, <Actor: Jaden Smith>]>
+
+if __name__ == "__main__":
+    main()
