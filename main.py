a2f148f61bf34ae1ed68c3492ec27bc78ce1c2e9
Ngayka <ngayka@gmail.com>
2025-03-19 16:50:04 +0200

    Solution

--- main.py
+++ main.py
@@ -5,24 +5,33 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    actors = Actor.objects.filter(
-        last_name="Smith",
-    ).order_by(
-        "first_name",)
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
+
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Keanu", last_name="Reeves").update(first_name="Keanu", last_name="Reeves")
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+    actors = Actor.objects.filter(last_name="Smith").order_by("first_name")
     return actors
 
-    genres = Genre.objects.all()
-    return genres
 
 print(main())
-# <QuerySet [<Actor: Jaden Smith>, <Actor: Will Smith>]>
 
 print(Genre.objects.all())
-# <QuerySet [<Genre: Western>, <Genre: Drama>]>
 
 print(Actor.objects.all())
-# <QuerySet [<Actor: George Clooney>, <Actor: Keanu Reeves>, <Actor: Will Smith>, <Actor: Jaden Smith>]>
-
+#
 if __name__ == "__main__":
     main()
