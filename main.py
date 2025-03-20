44eaa14e72a737492a70febc9d402641f715a957
cemen <frodo.work@gmail.com>
2025-03-20 13:38:57 +0200

    refactored objects creation

--- main.py
+++ main.py
@@ -5,41 +5,27 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    # create
-    Genre.objects.create(
-        name="Western",
-    )
-    Genre.objects.create(
-        name="Action",
-    )
-    Genre.objects.create(
-        name="Dramma",
-    )
 
-    Actor.objects.create(
-        first_name="George",
-        last_name="Klooney",
-    )
-    Actor.objects.create(
-        first_name="Kianu",
-        last_name="Reaves",
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Keegan",
-    )
-    Actor.objects.create(
-        first_name="Will",
-        last_name="Smith",
-    )
-    Actor.objects.create(
-        first_name="Jaden",
-        last_name="Smith",
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Johansson",
-    )
+    genre_data_list = [
+        {"name": "Western"},
+        {"name": "Action"},
+        {"name": "Dramma"},
+    ]
+
+    for genre_data in genre_data_list:
+        Genre.objects.create(**genre_data)
+
+    actors_data_list =[
+        {"first_name": "George", "last_name": "Klooney"},
+        {"first_name": "Kianu", "last_name": "Reaves"},
+        {"first_name": "Scarlett", "last_name": "Keegan"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Jaden", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
+    ]
+
+    for actor_data in actors_data_list:
+        Actor.objects.create(**actor_data)
 
     # update
     Genre.objects.filter(
