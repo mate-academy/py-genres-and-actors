348476625edcfdee69c890f407c1b66b9eaa7ee9
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-03-20 22:04:56 +0200

    Solution

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    genres = ["Western", "Action", "Drama"]
+    genres = ["Western", "Action", "Dramma"]
     actors = [
-        ["George", "Klooney"],
-        ["Kianu", "Reaves"],
-        ["Scarlett", "Keegan"],
-        ["Will", "Smith"],
-        ["Jaden", "Smith"],
-        ["Scarlett", "Johansson"]
+        ["George Klooney"],
+        ["Kianu Reaves"],
+        ["Scarlett Keegan"],
+        ["Will Smith"],
+        ["Jaden Smith"],
+        ["Scarlett Johansson"]
     ]
     genres_to_update = {"Dramma": "Drama"}
     actors_to_update = [
@@ -21,22 +21,25 @@ def main() -> QuerySet:
         ["Kianu Reaves", "Keanu Reeves"]
     ]
     genres_to_delete = ["Action"]
-    actors_to_delete = ["Scarlett"]
+    actors_to_delete = [
+        ("Scarlett")
+    ]
     to_return = "Smith"
 
     # CREATE
     for genre in genres:
         Genre.objects.create(name=genre)
-        print(genre, "created!")
 
     for actor in actors:
-        Actor.objects.create(first_name=actor[0], last_name=actor[1])
-        print(actor, "created!")
+        full_name = actor[0].split()
+        Actor.objects.create(
+            first_name=full_name[0],
+            last_name=full_name[1]
+        )
 
     # UPDATE
     for key, value in genres_to_update.items():
         Genre.objects.filter(name=key).update(name=value)
-        print(key, "updated!", "It stated to ", value)
 
     for actor in actors_to_update:
         current_name = actor[0].split()
@@ -48,22 +51,15 @@ def main() -> QuerySet:
             first_name=new_name[0],
             last_name=new_name[1]
         )
-        print(actor[0], "updated!", "new name is ", new_name)
 
     # DELETE
     for genre in genres_to_delete:
         Genre.objects.filter(name=genre).delete()
-        print(genre, "deleted!")
 
     for actor in actors_to_delete:
         Actor.objects.filter(first_name=actor).delete()
-        print(actor, "deleted!")
 
     query_set = Actor.objects.filter(
         last_name=to_return
     ).order_by("first_name")
     return query_set
-
-
-if __name__ == "__main__":
-    print(main())
