0b8e4be37737871bdfaa96ec2e1678b67d17540b
Maksym Omelkov <omelkov85@gmail.com>
2025-03-21 09:24:18 +0200

    Cinema Hall (1.0)

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import init_django_orm  # noqa: F401
-
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    genres_ls = ["Western", "Action", "Dramma"]
+    for genre in genres_ls:
+        Genre.objects.create(name=genre)
+
+    actors_ls = [("George", "Klooney"), ("Kianu", "Reaves"),
+                 ("Scarlett", "Keegan"),
+                 ("Will", "Smith"), ("Jaden", "Smith"),
+                 ("Scarlett", "Johansson")]
+    for first_name, last_name in actors_ls:
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(
+        first_name="George", last_name="Clooney"
+    ).update(first_name="George Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
