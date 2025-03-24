3caf0507b8b89c4ec5002a848f33f856c0f92d17
Denys <djkjibyltybc@gmail.com>
2025-03-24 23:30:16 +0200

    solution to the problem, bug fix

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu" , last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett" , last_name="Keegan")
-    Actor.objects.create(first_name="Will" , last_name="Smith")
-    Actor.objects.create(first_name="Jaden" , last_name="Smith")
-    Actor.objects.create(first_name="Scarlett" , last_name="Johansson")
+    genres = [("Western",), ("Action",), ("Dramma",)]
+    for genre_name in genres:
+        Genre.objects.create(name=genre_name[0])
+    actors = [("George", "Klooney"), ("Kianu", "Reaves"),
+              ("Scarlett", "Keegan"), ("Will", "Smith"),
+              ("Jaden", "Smith"),
+              ("Scarlett", "Johansson")]
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George").update(last_name="Clooney")
     Actor.objects.filter(first_name="Kianu").update(
