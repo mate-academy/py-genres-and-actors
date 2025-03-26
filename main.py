b493b1970e6a40d930dedc27e1946663acf841c4
Windings-Lab <janton20001@gmail.com>
2025-03-26 16:39:58 +0200

    code fix based on Checklist recomendation

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
+genre_list = ["Western", "Action", "Dramma"]
+actor_list = [
+    {"first_name": "George", "last_name": "Klooney"},
+    {"first_name": "Kianu", "last_name": "Reaves"},
+    {"first_name": "Scarlett", "last_name": "Keegan"},
+    {"first_name": "Will", "last_name": "Smith"},
+    {"first_name": "Jaden", "last_name": "Smith"},
+    {"first_name": "Scarlett", "last_name": "Johansson"},
+]
+
+
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    for genre in genre_list:
+        Genre.objects.create(name=genre)
+
+    for actor in actor_list:
+        Actor.objects.create(
+            first_name=actor["first_name"],
+            last_name=actor["last_name"]
+        )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     (Actor.objects.filter(first_name="George", last_name="Klooney")
