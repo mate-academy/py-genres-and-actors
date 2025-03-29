2c949d883eb31e39d0f1c02bc5bd324591efbc26
Valdyslav <magnetto54@gmail.com>
2025-03-29 09:57:29 +0100

    Solution_2

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from django.db.models import QuerySet
 import init_django_orm  # noqa: F401
 from db.models import Genre, Actor
 
-
+genres = ["Western", "Action", "Dramma"]
+actors = [("George","Klooney"),("Kianu","Reaves"),("Scarlett","Keegan"),
+          ("Will","Smith"),("Jaden","Smith"), ("Scarlett", "Johansson")]
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    for genre in genres:
+        Genre.objects.create(name=genre)
+    for actor in actors:
+        for first_tile, second_tile in actor:
+            Actor.objects.create(first_name=first_tile, last_name=second_tile)
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
         first_name="George", last_name="Klooney").update(last_name="Clooney")
@@ -22,7 +20,3 @@ def main() -> QuerySet:
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == "__main__":
-    main()
