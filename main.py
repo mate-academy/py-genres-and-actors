b457803540b1c286c3d9e8f9a78c25bcabf35e8a
ValeriyIvanovich <98410772+ValeriyIvanovich@users.noreply.github.com>
2025-04-02 16:08:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,3 @@ def main() -> QuerySet:
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == "__main__":
-    main()
