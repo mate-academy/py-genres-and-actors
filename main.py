0b6072bac6c34f30ab36c813cb680427fbef1b03
Volodymir Shlikhta <shlikhta77@gmail.com>
2025-04-07 17:14:30 +0300

    DB for Genres and Actors on Python

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
-
-from db.models import Genre
-
-from db.models import Actor
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
 
-    #Genre.objects.create(name="Dramma",)
-    #Genre.objects.filter(name="Dramma").update(name="Drama")
-    #Genre.objects.filter(name="Action").delete()
-    print(Genre.objects.all())
+    genres = ["Action", "Comedy", "Drama"]
+    for genre_name in genres:
+        Genre.objects.get_or_create(name=genre_name)
 
-    #Actor.objects.create(first_name="Scarlett", last_name="Johansson",)
-    #Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
-    #Actor.objects.filter(first_name="Scarlett").delete()
-    Actor_filter = Actor.objects.filter(last_name="Smith").order_by("first_name")
-    #print(Actor.objects.all())
-    print(Actor_filter)
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+
+    Genre.objects.filter(name="Action").delete()
+
+    actors = [
+        {"first_name": "Keanu", "last_name": "Reeves"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
+        {"first_name": "Brad", "last_name": "Pitt"},
+    ]
+    for actor_data in actors:
+        Actor.objects.get_or_create(**actor_data)
 
 
-if __name__ == "__main__":
-    main()
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves"
+    )
+
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
