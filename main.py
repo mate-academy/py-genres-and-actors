2e476257be60c75e6e48fb11e21dda5577f48f8f
tima1 <tima0323199@gmail.com>
2025-04-18 21:16:14 +0300

    Solution

--- main.py
+++ main.py
@@ -2,6 +2,54 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+
 
 def main() -> QuerySet:
-    pass
+    name_genres = "Western", "Action", "Dramma"
+    name_actor = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+
+    for item  in name_genres:
+        Genre.objects.create(name=item)
+
+    for item_first, item_last in name_actor:
+        Actor.objects.create(
+            first_name=item_first,
+            last_name=item_last
+        )
+
+    Genre.objects.filter(name="Dramma"
+                         ).update(name="Drama")
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+    Genre.objects.filter(name="Action"
+                         ).delete()
+    Actor.objects.filter(
+        first_name="Scarlett").delete()
+    return Actor.objects.filter(
+        last_name="Smith"
+    ).order_by("first_name")
+
+
+if __name__ == "__main__":
+    main()
+
+    print(main())
+    # <QuerySet [<Actor: Jaden Smith>, <Actor: Will Smith>]>
+
+    print(Genre.objects.all())
+    # <QuerySet [<Genre: Western>, <Genre: Drama>]>
+
+    print(Actor.objects.all())
