9aa770025fb4512dfa287146c2d944d30e3c8202
RomanSapunGit <sapunroma23@gmail.com>
2025-04-24 20:56:30 +0200

    Final Solution

--- db/models.py
+++ db/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Genre(models.Model):
-    objects = models.Manager
+    objects = models.Manager()
     name = models.CharField(max_length=255)
 
 
 class Actor(models.Model):
-    objects = models.Manager
+    objects = models.Manager()
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main() -> QuerySet:
     (Actor.objects.filter(first_name="George")
      .update(last_name="Clooney"))
     (Actor.objects.filter(first_name="Kianu")
-     .update(first_name="Keanu", last_name="Reaves"))
+     .update(first_name="Keanu", last_name="Reeves"))
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
