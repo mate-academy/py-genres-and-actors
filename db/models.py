1ef9cc4b0bfa30c50fc2e2a356beb47ebcc3599a
RomanSapunGit <sapunroma23@gmail.com>
2025-04-24 20:50:21 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    objects = models.Manager
+    name = models.CharField(max_length=255)
+
+
+class Actor(models.Model):
+    objects = models.Manager
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    genres_list = ["Western", "Action", "Dramma"]
+    for genre in genres_list:
+        Genre.objects.create(name=genre)
+
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Scarlett", "Johansson"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+    ]
+
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu", last_name="Reaves")
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
