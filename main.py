cea6ba9442ebb1836395a806a9b731c981533789
Alexandr <oleksandrglushen2005@gmail.com>
2025-05-04 15:04:39 +0300

    fixed code 3

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main() -> QuerySet:
     Actor.objects.bulk_create(actors)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney"),
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
         first_name="Keanu", last_name="Reeves")
 
