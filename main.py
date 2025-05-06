21e32c67f5b52dcdc2c7a162630e1fbbd3364403
Danylo-D87 <quaestor88@gmail.com>
2025-05-06 19:10:47 +0300

    sol 2

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from django.db.models import QuerySet
 
 
 def main() -> QuerySet:
-
+    # Створення жанрів
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
 
+    # Створення акторів
     Actor.objects.create(first_name="George", last_name="Klooney")
     Actor.objects.create(first_name="Kianu", last_name="Reaves")
     Actor.objects.create(first_name="Scarlett", last_name="Keegan")
@@ -16,12 +17,17 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
+    # Оновлення
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney"
-                         ).update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves"
-                         ).update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+
+    # Видалення
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
+    # Повертаємо акторів з прізвищем "Smith", відсортованих за ім'ям
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
