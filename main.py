e6a917177c1d7903aa6db430bbc2e458c01fb93b
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-05-13 01:38:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
-from db.model import Genre, Actor
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
     # create
-    Genre.objects.create("Western")
-    Genre.objects.create("Action")
-    Genre.objects.create("Dramma")
-    Actor.objects.create("George", "Klooney")
-    Actor.objects.create("Kianu", "Reaves")
-    Actor.objects.create("Scarlett", "Keegan")
-    Actor.objects.create("Will", "Smith")
-    Actor.objects.create("Jaden", "Smith")
-    Actor.objects.create("Scarlett", "Johansson")
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
     # update
-    Genre.objects.filter(name="Dramma").update("Drama")
+    Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(last_name="Reeves")
     # delete
