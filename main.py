61b74bc4e9f489ed0ffabf3a287c3f765cb27d0b
Maksym-Filipchuk <maxfiliya@gmail.com>
2025-05-15 20:24:21 +0200

    sl4

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main() -> QuerySet:
         first_name="George", last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
-    ).update(first_name="Keanu", last_name="Reeves")
+        ).update(first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
