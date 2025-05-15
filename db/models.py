6fb6eac6bb9478e34f1cca26a029e8dff80065f3
Maksym-Filipchuk <maxfiliya@gmail.com>
2025-05-15 20:09:34 +0200

    sl

--- db/models.py
+++ db/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Genre(model.Model):
+    name = models.CharField(max_length=255)
+    
+class Actor(model.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
--- main.py
+++ main.py
@@ -2,6 +2,34 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from django.db.models import Genre, Actor
+
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.create(name='Western')
+    Genre.objects.create(name='Action')
+    Genre.objects.create(name='Dramma')
+    
+
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    
+      # Оновлення
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
+
+    # Видалення
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # Повертаємо акторів з прізвищем "Smith", відсортованих за ім'ям
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
