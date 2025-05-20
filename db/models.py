c2c5a4278a59ea51733013bbc199cd579e110aae
Vitalik <lg.mobile0808@gmail.com>
2025-05-20 18:43:34 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ('first_name', 'last_name')
--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import init_django_orm  # noqa: F401
 
-from django.db.models import QuerySet
+from db.models import Actor, Genre
 
 
-def main() -> QuerySet:
-    pass
+def main():
+    Genre.objects.bulk_create([
+        Genre(name="Western"),
+        Genre(name="Action"),
+        Genre(name="Dramma"),
+    ])
+
+    Actor.objects.bulk_create([
+        Actor(first_name="George", last_name="Clooney"),
+        Actor(first_name="Keanu", last_name="Reeves"),
+        Actor(first_name="Scarlett", last_name="Keegan"),
+        Actor(first_name="Will", last_name="Smith"),
+        Actor(first_name="Jaden", last_name="Smith"),
+        Actor(first_name="Scarlett", last_name="Johansson"),
+    ])
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney"
+                         ).update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves"
+                         ).update(first_name="Keanu", last_name="Reeves")
+    actors_named_smith = Actor.objects.filter(last_name="Smith").order_by("first_name")
+    return actors_named_smith
+
+
+if __name__ == '__main__':
+    main()
+
