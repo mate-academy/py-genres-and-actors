45e7809ca5cdb27fbb5e6af6ac18c0617244a517
Ihor Borunov <borunov65@gmail.com>
2025-06-02 21:04:36 +0200

    Solution

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ actor_names = [
 def main() -> QuerySet:
     for genre_name in genre_names:
         Genre.objects.create(name=genre_name)
-    for actor_first_name, actor_last_name in actor_names:
+    for actor_f_name, actor_l_name in actor_names:
         (Actor.objects.create
-         (first_name=actor_first_name, last_name=actor_last_name))
+         (first_name=actor_f_name, last_name=actor_l_name))
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     (Actor.objects.filter(first_name="Kianu", last_name="Reaves").update
