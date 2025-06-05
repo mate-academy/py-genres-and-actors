c7370d5d38053fb53286c0340a9ab39a3ad38915
= <=>
2025-06-05 15:08:04 +0300

    implement py-genres-and-actors

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from db.models import Genre, Actor
+from django.db.models import QuerySet
 
 
-def main():
+def main() -> QuerySet:
     # CREATE genres
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
-    Genre.objects.create(name="Drama")
+    Genre.objects.create(name="Dramma")
 
     # CREATE actors
     Actor.objects.create(first_name="George", last_name="Klooney")
@@ -19,12 +20,14 @@ def main():
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
     # UPDATE George Klooney → George Clooney
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney"
+    ).update(last_name="Clooney")
 
     # UPDATE Kianu Reaves → Keanu Reeves
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
-        first_name="Keanu", last_name="Reeves"
-    )
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
 
     # DELETE genre "Action"
     Genre.objects.filter(name="Action").delete()
