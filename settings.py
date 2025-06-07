f5561def3f7f14b33dd2fe0d1d2a5f51b22b941e
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-06-07 19:18:15 +0200

    Solution6

--- settings.py
+++ settings.py
@@ -9,11 +9,11 @@ SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'SERIALIZE': False,
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "TEST": {
+            "SERIALIZE": False,
         },
     }
 }
