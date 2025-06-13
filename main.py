6b5522c47049914c1fc0e7d05a4ae8e667469f7f
Vlad <7nomer7@gmail.com>
2025-06-13 22:00:53 +0300

    Solution

--- main.py
+++ main.py
@@ -17,11 +17,17 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="George", last_name="Klooney").update(
+        last_name="Clooney"
+    )
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(first_name="Scarlett", last_name="Keegan").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     total = Actor.objects.filter(last_name="Smith").order_by("first_name")
     return total
@@ -29,5 +35,5 @@ def main() -> QuerySet:
 
 if __name__ == "__main__":
     print(main())
-    # print(Genre.objects.all())
-    # print(Actor.objects.all())
+    print(Genre.objects.all())
+    print(Actor.objects.all())
