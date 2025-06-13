80a4067ba6ecb3d64fc9ad453b3f902f94a4751d
Vlad <7nomer7@gmail.com>
2025-06-13 22:17:52 +0300

    Solution

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main() -> QuerySet:
     )
 
     Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(first_name="Scarlett").delete()
+    Actor.objects.filter(first_name="Scarlett", last_name="Johansson").delete()
 
     total = Actor.objects.filter(last_name="Smith").order_by("first_name")
     return total
