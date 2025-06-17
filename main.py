6e5320a67e4b2356f34faf9a7ea50773f6fba649
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-06-17 14:32:59 -0300

    'Solution'

--- main.py
+++ main.py
@@ -60,4 +60,4 @@ def main() -> QuerySet:
 
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(last_name="Smith").order_by("fisrt_name")
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
