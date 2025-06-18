de269fcf289ec3501d28e16ccecdc37a78cb19a9
Alexey <alopar632@gmail.com>
2025-06-18 14:29:18 +0300

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from db.models import Genre, Actor
 
 
-def main():
+def main() -> None:
     genres = ["Western", "Action", "Dramma"]
     for name in genres:
         Genre.objects.create(name=name)
@@ -12,24 +12,24 @@ def main():
         ("Scarlett", "Keegan"),
         ("Will", "Smith"),
         ("Jaden", "Smith"),
-        ("Scarlett", "Johansson")
+        ("Scarlett", "Johansson"),
     ]
     for first_name, last_name in actors:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
-    Genre.objects.filter(name='Dramma').update(name='Drama')
+    Genre.objects.filter(name="Dramma").update(name="Drama")
 
     Actor.objects.filter(
-        first_name='George',
-        last_name='Klooney'
-    ).update(last_name='Clooney')
+        first_name="George",
+        last_name="Klooney",
+    ).update(last_name="Clooney")
 
     Actor.objects.filter(
-        first_name='Kianu',
-        last_name='Reaves'
-    ).update(first_name='Keanu', last_name='Reeves')
+        first_name="Kianu",
+        last_name="Reaves",
+    ).update(first_name="Keanu", last_name="Reeves")
 
-    Genre.objects.filter(name='Action').delete()
-    Actor.objects.filter(first_name='Scarlett').delete()
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(last_name='Smith').order_by('first_name')
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
