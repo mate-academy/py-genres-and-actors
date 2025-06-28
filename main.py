05acdf51c59024f83e37fa3c759a864051a683be
Oleksandr Tarnavsky <sasha.tarnavsky@gmail.com>
2025-06-28 20:07:35 +0300

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-import init_django_orm  # noqa: F401
-
-from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
-def main() -> QuerySet:
-    pass
+def main() -> None:
+    # Create genres
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+
+    # Create actors/actresses
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    # Update records
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+
+    # Delete genre Action
+    Genre.objects.filter(name="Action").delete()
+
+    # Delete actresses with first_name Scarlett
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # Return queryset of actors with last_name Smith, ordered by first_name
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
