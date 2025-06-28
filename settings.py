0898f047a4da86be571411f3bbdb883cd9bb999a
Oleksandr Tarnavsky <sasha.tarnavsky@gmail.com>
2025-06-28 20:32:07 +0300

    Solution

--- settings.py
+++ settings.py
@@ -18,12 +18,12 @@ DATABASES = {
 USE_TZ = False
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # ... інші встановлені додатки, якщо є
     'db',  # <--- Ось тут додайте ваш додаток 'db'
 ]
