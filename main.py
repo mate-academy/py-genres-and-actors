134c0fc6aa52d7567927bc9db40bb0b9e18cf9b3
yarik <super.racing@ukr.net>
2025-07-29 00:22:36 +0300

    fixed some mistakes

--- main.py
+++ main.py
@@ -21,10 +21,6 @@ def main() -> QuerySet:
     Actor.objects.filter(first_name="Kianu",
                          last_name="Reaves").update(first_name="Keanu",
                                                     last_name="Reeves")
-    Genre.objects.delete(name="Action")
-    Actor.objects.delete(first_name="Scarlett")
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == "__main__":
-    main()
