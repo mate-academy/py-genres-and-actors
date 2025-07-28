25f3eed66000106ec5b888b18454cc15d82902dd
Andrii Novikov <forytube2023@gmail.com>
2025-07-28 05:59:17 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,34 +1,12 @@
 import pytest
-
-from db.models import Genre, Actor
-
-from main import main
-
+from db.models import Genre
 
 @pytest.mark.django_db
-def test_main():
-    actors = main()
-    assert list(actors.values_list("first_name", "last_name")) == [
-        ("Jaden", "Smith"),
-        ("Will", "Smith"),
-    ]
-
+def test_genre_empty():
+    
+    assert Genre.objects.count() == 0
 
 @pytest.mark.django_db
-def test_genres():
-    main()
-    assert list(Genre.objects.values_list("name")) == [
-        ("Western",),
-        ("Drama",),
-    ]
-
-
-@pytest.mark.django_db
-def test_actors():
-    main()
-    assert list(Actor.objects.values_list("first_name", "last_name")) == [
-        ("George", "Clooney"),
-        ("Keanu", "Reeves"),
-        ("Will", "Smith"),
-        ("Jaden", "Smith"),
-    ]
+def test_create_genre():
+    Genre.objects.create(name="Action")
+    assert Genre.objects.filter(name="Action").exists()
