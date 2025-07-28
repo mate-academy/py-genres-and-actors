5be11a0354e9878467fc1e7eff80b44d47a043db
Andrii Novikov <forytube2023@gmail.com>
2025-07-28 06:03:19 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
-class Genre(models.Model):
-    name = models.CharField(max_length=100)
 
-    def __str__(self):
+class Genre(models.Model):
+    name: str = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
         return self.name
+
+
+class Actor(models.Model):
+    name: str = models.CharField(max_length=100)
+    surname: str = models.CharField(max_length=100)
+    age: int = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.surname}"
