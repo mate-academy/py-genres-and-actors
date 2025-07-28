06fedc27344f7d8c3ceb015b9da4faf7cc0c26c8
yarik <super.racing@ukr.net>
2025-07-29 00:00:49 +0300

    created models and queries

--- db/models.py
+++ db/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_len=255)
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_len=255)
+    last_name = models.CharField(max_len=255)
