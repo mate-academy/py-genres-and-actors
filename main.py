9937863f37fe80294c9ef09b63858d23da8d42a8
Bohdan Kolisnyk <bcolisnyk04@gmail.com>
2025-07-30 09:48:35 +0300

    АÐFix_flake

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
-
-from db.models import  Genre, Actor
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
@@ -22,10 +21,14 @@ def main() -> QuerySet:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
-        first_name="Keanu", last_name="Reeves"
-    )
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).update(first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
