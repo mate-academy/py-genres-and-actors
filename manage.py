86a40699065b0b70f54fff31c1eacc95e2a8eae5
Andrii Novikov <forytube2023@gmail.com>
2025-08-01 15:37:45 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,8 +4,9 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genres_and_actors.settings')
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "genres_and_actors.settings"
+    )
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-    
