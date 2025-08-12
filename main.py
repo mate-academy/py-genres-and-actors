cf858e34489db642fd2e648d2b50be87098b1d25
Your Name <stas101203rakitin@gmail.com>
2025-08-12 15:05:06 +0300

    Solution 2

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    # create
-    Genre.objects.bulk_create([
-        Genre(name="Western"),
-        Genre(name="Action"),
-        Genre(name="Dramma")
-    ])
-    Actor.objects.bulk_create([
-        Actor(first_name="George", last_name="Klooney"),
-        Actor(first_name="Kianu", last_name="Reaves"),
-        Actor(first_name="Scarlett", last_name="Keegan"),
-        Actor(first_name="Will", last_name="Smith"),
-        Actor(first_name="Jaden", last_name="Smith"),
-        Actor(first_name="Scarlett", last_name="Johansson"),
-    ])
+    # create (цикли замість bulk_create)
+    for name in ["Western", "Action", "Dramma"]:
+        Genre.objects.create(name=name)
+
+    for first_name, last_name in [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
 
     # update
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(first_name="Kianu").update(
-        first_name="Keanu", last_name="Reeves")
+        first_name="Keanu", last_name="Reeves"
+    )
 
     # delete
     Genre.objects.filter(name="Action").delete()
