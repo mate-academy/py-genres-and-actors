6fbf08585367763a7e2ba1a8a5a5c29594a5aec5
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-08-20 13:22:25 +0200

    Solution

--- main.py
+++ main.py
@@ -2,6 +2,53 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Drama")
+
+    Actor.objects.create(
+        first_name="George",
+        last_name="Klooney",
+    )
+    Actor.objects.create(
+        first_name="Kianu",
+        last_name="Reaves",
+    )
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Keegan",
+    )
+    Actor.objects.create(
+        first_name="Will",
+        last_name="Smith",
+    )
+    Actor.objects.create(
+        first_name="Jaden",
+        last_name="Smith",
+    )
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Johansson",
+    )
+
+    genre = Genre.objects.get(name="Drama")
+    genre.name = "Drama"
+    genre.save()
+
+    actor = Actor.objects.get(first_name="George", last_name="Klooney")
+    actor.name = "George"
+    actor.save()
+
+    actor = Actor.objects.get(first_name="Kianu", last_name="Reaves")
+    actor.name = "Kianu"
+    actor.save()
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    actors_smith = Actor.objects.filter(first_name="Smith").order_by("first_name")
+    return actors_smith
--- settings.py
+++ settings.py
@@ -2,6 +2,12 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    "db_genre",  # наш застосунок з моделями
+]
 
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
