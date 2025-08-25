31a33ad914001eaf9e841755346320c16ecfd4ea
Tomasz Herman <tomekh2015@gmail.com>
2025-08-25 13:20:29 +0200

    Solution

--- main.py
+++ main.py
@@ -4,4 +4,23 @@ from django.db.models import QuerySet
 
 
 def main() -> QuerySet:
-    pass
+    Western = genre.objects.create(name="Western")
+    Action = genre.objects.create(name="Action")
+    Drama = genre.objects.create(name="Drama")
+
+    George = Actor.objects.create(first_name="George", last_name="Klooney")
+    Kianu = Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Scarlett = Actress.objects.create(first_name="Scarlett", last_name="Keegan")
+    Will = Actor.objects.create(first_name="Will", last_name="Smith")
+    Jaden = Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Scarlett = Actress.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(last_name="Reeves")
+
+    genre.objects.filter(name="Action").delete()
+    Actress.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
