f0142bf5f94c8b1eaba69ebed2ab3dd9ffe20b6c
Vitalii Mushta <vitaliimushta1993@gmail.com>
2025-09-09 17:52:00 +0200

    Solutinon !!!!!

--- db/models.py
+++ db/models.py
@@ -5,6 +5,13 @@ class Genre(models.Model):
     name = models.CharField(max_length=255)
 
 
+    def __str__(self):
+        return self.name
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
