772d8bd70204646b8fbc726153c3350d4eb744de
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2025-09-12 19:03:19 +0300

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-import init_django_orm  # noqa: F401
+import init_django_orm # noqa: F401
 
-from django.db.models import QuerySet
+from django.db.models import QuerySet, IntegerField, Case, When
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    genres = ["Western", "Drama"]
+    for genre in genres:
+        Genre.objects.create(name=genre)
+
+    actors = [
+        ("George", "Clooney"),
+        ("Keanu", "Reeves"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+    ]
+
+    for first_name, last_name in actors:
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name
+        )
+
+
+    return Actor.objects.filter(
+        first_name__in=["Jaden", "Will"],
+        last_name="Smith"
+    ).order_by(
+        Case(
+            When(first_name="Jaden", then=0),
+            When(first_name="Will", then=1),
+            output_field=IntegerField(),
+        )
+    )
+
