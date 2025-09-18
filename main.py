651027c2768e25e1d4fa7e82d8f4d211717638d5
Mateusz <szanieckimateusz@gmail.com>
2025-09-18 14:46:22 +0200

    Added delete and update queries

--- main.py
+++ main.py
@@ -14,14 +14,27 @@ def main() -> QuerySet:
         {"first_name": "Will", "last_name": "Smith"},
         {"first_name": "Jaden", "last_name": "Smith"},
         {"first_name": "Scarlett", "last_name": "Johansson"},
-        ]
+    ]
 
     for genre in genres:
         Genre.objects.create(name=genre)
 
     for actor in actors:
-        Actor.objects.create(first_name=actor["first_name"], last_name=actor["last_name"])
+        Actor.objects.create(
+            first_name=actor["first_name"],
+            last_name=actor["last_name"]
+        )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
+    george_clooney = Actor.objects.get(last_name="Klooney")
+    george_clooney.last_name = "Clooney"
+    george_clooney.save()
+    keanu = Actor.objects.get(first_name="Kianu")
+    keanu.first_name = "Keanu"
+    keanu.last_name = "Reeves"
+    keanu.save()
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
