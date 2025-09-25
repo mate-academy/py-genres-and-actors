b970c7efb44dfdcdcfa5afd77d011921acc12a7c
Samuel Yudi <toyotaa038@gmail.com>
2025-09-25 17:21:37 -0300

    solution

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-import init_django_orm  # noqa: F401
+from db.models import Genre, Actor
+from django.db import transaction
 
-from django.db.models import QuerySet
+def main():
+    with transaction.atomic():
+        # Criar gêneros
+        Genre.objects.create(name="Western")
+        Genre.objects.create(name="Ação")
+        Genre.objects.create(name="Drama")
 
+        # Criar atores e atrizes
+        Actor.objects.create(first_name="George", last_name="Klooney")
+        Actor.objects.create(first_name="Kianu", last_name="Reaves")
+        Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+        Actor.objects.create(first_name="Will", last_name="Smith")
+        Actor.objects.create(first_name="Jaden", last_name="Smith")
+        Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
-def main() -> QuerySet:
-    pass
+        # Atualizar registros
+        Genre.objects.filter(name="Drama").update(name="Drama")  # aparentemente não muda nada
+        Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
+        Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+
+        # Excluir registros
+        Genre.objects.filter(name="Ação").delete()
+        Actor.objects.filter(first_name="Scarlett").delete()
+
+        # Retornar QuerySet de atores com last_name "Smith" ordenados por first_name
+        result = Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+    return result
