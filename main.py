655d39c669a729338e11c48ded79c62cc9987fa6
vasylhladyshev404 <vasyl@nextgear.nl>
2025-10-04 11:31:38 +0200

    1

--- main.py
+++ main.py
@@ -21,29 +21,29 @@ def main() -> QuerySet:
     for first_name, last_name in actors:
         Actor.objects.create(
             first_name=first_name,
-            last_name=last_name,
+            last_name=last_name
         )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
 
     Actor.objects.filter(
-        first_name="George"
+        first_name="George",
         last_name="Klooney"
     ).update(
         last_name="Clooney"
     )
 
     Actor.objects.filter(
-        first_name="Kianu"
+        first_name="Kianu",
         last_name="Reaves"
     ).update(
-        first_name="Keanu",
+        first_name="Keanu",,
         last_name="Reeves",
     )
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(
-        first_name="Scarlett"
+        first_name="Scarlett",
         last_name="Keegan"
     ).delete()
 
