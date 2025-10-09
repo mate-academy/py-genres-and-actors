40e8cf7d070bf17f76b07fe041b7a591e2d00d4e
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-10-09 15:23:37 +0700

    devel

--- db/models.py
+++ db/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from django.core.management import call_command
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-
-
-call_command("makemigrations", "db", interactive=False)
-call_command("migrate", interactive=False)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
-
-import init_django_orm  # noqa: F401
 from db.models import Genre, Actor
 
 
@@ -17,21 +15,29 @@ def main() -> QuerySet:
         ("Scarlett", "Keegan"),
         ("Will", "Smith"),
         ("Jaden", "Smith"),
-        ("Scarlett", "Johansson"),]:
-            Actor.objects.create(
-            first_name = fname,
-            last_name = lname,
-    )
+        ("Scarlett", "Johansson"),
+    ]:
+        Actor.objects.create(
+            first_name=fname,
+            last_name=lname,
+        )
     Genre.objects.filter(
-        name = "Dramma"
+        name="Dramma"
     ).update(
-        name = "Drama"
+        name="Drama"
     )
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    actors_smiths = Actor.objects.filter(last_name="Smith").all().order_by("first_name")
+    actors_smiths = Actor.objects.filter(
+        last_name="Smith").all().order_by("first_name")
 
     return actors_smiths
