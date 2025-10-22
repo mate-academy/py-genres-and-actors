b7b93ad75dda1db1269fde6fa4587865dda9b6af
gfray-castilho <gfraycastilho@gmail.com>
2025-10-22 17:25:59 -0300

    solved

--- db/models.py
+++ db/models.py
@@ -6,6 +6,7 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
