95d70700954c2e0d11499a2ce7a1184c71177207
gfray-castilho <gfraycastilho@gmail.com>
2025-10-22 17:14:04 -0300

    solved

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import init_django_orm  # noqa: F401
 from django.db.models import QuerySet
-from db.models import Genre,Actor
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
+
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
@@ -15,7 +16,7 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu",last_name="Reeves")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
