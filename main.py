502c68cae71473861dd02cda6d5f8f31d0d6519d
Maksym Dazidov <maksymdazidov@gmail.com>
2025-11-08 17:54:55 +0100

    big code 2

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ def main() -> QuerySet:
                          ).update(first_name="Keanu", last_name="Reeves")
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
-    query = Actor.objects.filter(first_name="Smith").order_by("first_name")
+    query = Actor.objects.filter(last_name="Smith").order_by("first_name")
     return query
