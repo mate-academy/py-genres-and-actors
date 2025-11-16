7153484c9e23ab55139319fe335e96a1f83b1187
Alex <zigzaksash@gmail.com>
2025-11-17 00:01:46 +0100

    Solution

--- main.py
+++ main.py
@@ -11,20 +11,12 @@ def main() -> QuerySet:
     for genre in genres:
         final_genre = Genre(name=genre)
         final_genre.save()
-    actors = ["George Klooney", "Kianu Reaves", "Will Smith", "Jaden Smith"]
-    for actor in actors:
-        actor = actor.split()
+    actors = ["George Klooney", "Kianu Reaves", "Will Smith",
+              "Jaden Smith", "Scarlett Keegan" , "Scarlett Johansson"]
+    for fn, ln in actors:
         final_actor = Actor(
-            first_name=actor[0], last_name=actor[1])
+            first_name=fn, last_name=ln)
         final_actor.save()
-    actresses = ["Scarlett Keegan" , "Scarlett Johansson"]
-    for actress in actresses:
-        actress = actress.split()
-        final_actress = (Actor(first_name=actress[0],
-                               last_name=actress[1]
-                               )
-                         )
-        final_actress.save()
     # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(
