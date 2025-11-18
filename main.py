ecfb27f713150335881aab9396ca03b8e574f987
Thaís <parisethais@gmail.com>
2025-11-18 03:24:40 -0300

    fixed git push origin develop q

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+
 from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    genres_data = ["Western", "Action", "Dramma"]
+    for genre_name in genres_data:
+        Genre.objects.create(name=genre_name)
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    actors_data = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors_data:
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+        )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
+
     Actor.objects.filter(
         first_name="George",
         last_name="Klooney",
     ).update(last_name="Clooney")
 
-    Actor.objects.filter(
-        first_name="Kianu",
-        last_name="Reaves",
-    ).update(
-        first_name="Keanu",
-        last_name="Reeves",
-    )
     Actor.objects.filter(
         first_name="Kianu",
         last_name="Reaves",
