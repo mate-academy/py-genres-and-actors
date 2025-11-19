7cbfb33702e5492a2bfdd198e583de1f2c1a2b2c
Dawid <kucharzjaw@gmail.com>
2025-11-19 18:32:54 +0100

    Solution

--- main.py
+++ main.py
@@ -6,20 +6,37 @@ from db.models import Genre, Actor
 
 def main() -> QuerySet:
     # CREATE
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    actors = [
+        ("George", "Klooney"),
+        ("Keanu", "Reeves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reeves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
+    genres = ["Western", "Action", "Dramma"]
+
+    for name in genres:
+        Genre.objects.create(name=name)
+    # Genre.objects.create(name="Western")
+    # Genre.objects.create(name="Action")
+    # Genre.objects.create(name="Dramma")
+    #
+    # Actor.objects.create(first_name="George", last_name="Klooney")
+    # Actor.objects.create(first_name="Kianu", last_name="Reeves")
+    # Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    # Actor.objects.create(first_name="Will", last_name="Smith")
+    # Actor.objects.create(first_name="Jaden", last_name="Smith")
+    # Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     # UPDATE
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George").update(last_name="Clooney")
+    (Actor.objects.filter(first_name="George", last_name="Klooney")
+     .update(last_name="Clooney"))
     (Actor.objects.filter(first_name="Kianu", last_name="Reeves")
      .update(first_name="Keanu", last_name="Reeves"))
 
@@ -29,3 +46,5 @@ def main() -> QuerySet:
 
     # RETURN
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+
