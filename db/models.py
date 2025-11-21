95a8915486985136ef90b49f07c3e4d238099a50
Nikita <hkorobko@gmail.com>
2025-11-22 01:14:30 +0200

    Fixed Genres-and-Actors

--- db/models.py
+++ db/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Genre(models.Model):
-    name =models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
