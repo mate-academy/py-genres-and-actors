871a438c51dd029d3db855be1647c83634f49948
Yan Tumar <jantumar.unity@gmail.com>
2025-11-24 15:38:23 +0200

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
-import init_django_orm  # noqa: F401
+# main.py
 
+import init_django_orm  # noqa: F401
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    genres_to_create = [
+        ("Western",),
+        ("Action",),
+        ("Dramma",),
+    ]
+    for name_tuple in genres_to_create:
+        Genre.objects.create(name=name_tuple[0])
+
+    actors_to_create = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors_to_create:
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name
+        )
+
+    drama_genre = Genre.objects.get(name="Dramma")
+    drama_genre.name = "Drama"
+    drama_genre.save()
+
+    george = Actor.objects.get(first_name="George", last_name="Klooney")
+    george.last_name = "Clooney"
+    george.save()
+
+    kianu = Actor.objects.get(first_name="Kianu", last_name="Reaves")
+    kianu.first_name = "Keanu"
+    kianu.last_name = "Reeves"
+    kianu.save()
+    Genre.objects.get(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    smith_actors = Actor.objects.filter(
+        last_name="Smith"
+    ).order_by(
+        "first_name"
+    )
+
+    return smith_actors
