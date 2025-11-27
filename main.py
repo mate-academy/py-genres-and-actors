3db6ab623ea827e11c4686d3e628f7c353862ce6
Nick <nikitosi3012@gmail.com>
2025-11-27 05:57:58 +0200

    Flake8 fix

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 def main() -> QuerySet:
-    western = Genre.objects.create(name="Western")
+    Genre.objects.create(name="Western")
     action = Genre.objects.create(name="Action")
     drama = Genre.objects.create(name="Drama")
 
-    george_clooney = Actor.objects.create(first_name="George", last_name="Klooney")
-    keanu_reeves = Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    scarlett_keegan = Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    will_smith = Actor.objects.create(first_name="Will", last_name="Smith")
-    jaden_smith = Actor.objects.create(first_name="Jaden", last_name="Smith")
-    scarlett_johanson = Actor.objects.create(first_name="Scarlett", last_name="Johanson")
+    george_clooney = Actor.objects.create(
+        first_name="George",
+        last_name="Klooney")
+    keanu_reeves = Actor.objects.create(
+        first_name="Kianu",
+        last_name="Reaves")
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Keegan")
+    Actor.objects.create(
+        first_name="Will",
+        last_name="Smith")
+    Actor.objects.create(
+        first_name="Jaden",
+        last_name="Smith")
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Johanson")
 
     drama.name = "Drama"
     drama.save()
