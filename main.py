babefb4b5e3ba1f92b726ed19f5dcf8716ab8634
TimoteoRJanzen <timoteojanzen123@gmail.com>
2025-11-28 11:02:56 -0300

    fix

--- main.py
+++ main.py
@@ -4,33 +4,35 @@ from django.db.models import QuerySet
 
 from db.models import Genre, Actor
 
+
 def main() -> QuerySet:
     list_of_genres = ["Western", "Action", "Dramma"]
     list_of_actors = [
-        "George Klooney",
-        "Kianu Reaves",
-        "Scarlett Keegan",
-        "Will Smith",
-        "Jaden Smith",
-        "Scarlett Johansson"
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
     ]
 
     for genre in list_of_genres:
-        Genre.objects.create(name = genre)
+        Genre.objects.create(name=genre)
 
-    for actor in list_of_actors:
-        first_name, last_name = actor.split()
+    for first_name, last_name in list_of_actors:
         Actor.objects.create(
-            first_name = first_name,
-            last_name = last_name
+            first_name=first_name,
+            last_name=last_name
         )
 
-    Genre.objects.filter(name="Dramma").update(name= "Drama")
-    Actor.objects.filter(last_name = "Klooney").update(last_name ="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(first_name = "Keanu")
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu").update(first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    filtered_actors = Actor.objects.filter(last_name = "Smith").order_by("first_name")
+    filtered_actors = Actor.objects.filter(
+        last_name="Smith").order_by("first_name")
     return filtered_actors
