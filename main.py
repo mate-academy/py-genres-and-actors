7cab921f11b416634691066b293f39417b333d68
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-11-28 21:55:58 +0100

    first one?

--- main.py
+++ main.py
@@ -2,6 +2,37 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+
 
 def main() -> QuerySet:
-    pass
+
+    # creating the genres and actors
+    genres_list = ["Western", "Action", "Dramma"]
+    for genre in genres_list:
+        Genre.objects.create(name=genre)
+
+    actors_list = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+    for first_name, last_name in actors_list:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
+    # update
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu").update(
+        first_name="Keanu", last_name="Reeves"
+    )
+
+    # delete
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # return
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
