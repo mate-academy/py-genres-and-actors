70845106375bfed5ead1a9fed0051910c09ed0c6
Qbaldo <durowski@gmail.com>
2025-12-05 22:16:25 +0100

    solution loops

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    names = ["Western", "Action", "Dramma"]
+    for name in names:
+        Genre.objects.create(name=name)
+    actors = [
+        "George Klooney",
+        "Kianu Reaves",
+        "Scarlett Keegan",
+        "Will Smith",
+        "Jaden Smith",
+        "Scarlett Johansson"
+    ]
+    for actor in actors:
+        actor_split = actor.split(" ")
+        Actor.objects.create(first_name=actor_split[0],
+                             last_name=actor_split[1])
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George").update(last_name="Clooney")
