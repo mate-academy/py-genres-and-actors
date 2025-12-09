4eba9473656e4d3ae23459722a05c2b53ee0695e
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-12-09 17:12:32 +0100

    3rd fix

--- settings.py
+++ settings.py
@@ -1,36 +1,34 @@
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-SECRET_KEY = 'super-secret-key-just-for-testing'
+SECRET_KEY = "super-secret-key-just-for-testing"
 
 DEBUG = False
 
 ALLOWED_HOSTS = []
-
-# KLUCZOWE USTAWIENIE: Rejestracja aplikacji
 INSTALLED_APPS = [
-    # Wymagane podstawowe komponenty Django do poprawnego działania ORM
-    'django.contrib.contenttypes',
-    'django.contrib.auth',
+    # Poprawka Q000: użycie podwójnych cudzysłowów
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
 
-    # Twoja aplikacja (poprawna ścieżka do klasy konfiguracyjnej DbConfig)
+    # Poprawka Q000: użycie podwójnych cudzysłowów
     "db.apps.DbConfig",
 ]
 
 DATABASES = {
     "default": {
+        # Poprawka Q000: użycie podwójnych cudzysłowów
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
-# Konfiguracja dla pytest-django
-ROOT_URLCONF = 'main'
+# Poprawka Q000: użycie podwójnych cudzysłowów
+ROOT_URLCONF = "main"
 
 USE_TZ = False
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SILENCED_SYSTEM_CHECKS = ["urls.W002"]
+# W292: Została dodana nowa linia na końcu pliku002"]
