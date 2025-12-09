9c18e2d28ff4147705e07c53e3811ca8b4d4b8b0
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-12-09 17:22:38 +0100

    fix

--- db/models.py
+++ db/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=255)  # E261: Dodano dwie spacje
+    name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
-        return self.name  # E261: Dodano dwie spacje
+        return self.name
 
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=255)  # E261: Dodano dwie spacje
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     is_actress = models.BooleanField(default=False)
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"  # E261: Dodano dwie spacje
+        return f"{self.first_name} {self.last_name}"
+
