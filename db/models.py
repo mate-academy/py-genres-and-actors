aa612b823c3cd67cef65a6fe0f5baae3a6542847
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-12-09 17:19:06 +0100

    flake8/models/fix

--- db/models.py
+++ db/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
-class Genre(models.Model):
+
+class Genre(models.Model): # E302: 2 puste linie
     name = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str: # ANN204: Dodano adnotację typu
         return self.name
 
-class Actor(models.Model):
+
+class Actor(models.Model): # E302: 2 puste linie
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    # Pole, które było przyczyną błędu Type Error w testach
     is_actress = models.BooleanField(default=False)
 
-    def __str__(self):
+    def __str__(self) -> str: # ANN204: Dodano adnotację typu
         return f"{self.first_name} {self.last_name}"
