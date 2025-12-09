db6bd3cef13b9a8b688078fc9574d20768da5457
Ihor Zasimenko <boxletter2011@gmail.com>
2025-12-09 19:27:21 +0200

    fixed code - add loop

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet[Actor]:
-    Genre.objects.get_or_create(name="Western")
-    Genre.objects.get_or_create(name="Action")
-    Genre.objects.get_or_create(name="Drama")
+    genres = ["Western", "Action", "Dramma"]
+    for name in genres:
+        Genre.objects.get_or_create(name=name)
 
-    Actor.objects.get_or_create(first_name="George", last_name="Clooney")
-    Actor.objects.get_or_create(first_name="Keanu", last_name="Reeves")
-    Actor.objects.get_or_create(first_name="Will", last_name="Smith")
-    Actor.objects.get_or_create(first_name="Jaden", last_name="Smith")
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first, last in actors:
+        Actor.objects.get_or_create(first_name=first, last_name=last)
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     queryset: QuerySet[Actor] = (
         Actor.objects.filter(last_name="Smith").order_by("first_name")
     )
-
     return queryset
