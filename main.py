42a60a1d1661904e98637880957eb7fcb3220937
Hanna Svirina <26anna07@ukr.net>
2025-12-13 22:14:54 +0100

    'Solution'

--- main.py
+++ main.py
@@ -21,12 +21,23 @@ def main() -> QuerySet:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George").update(last_name="Clooney")
+
     Actor.objects.filter(
-        first_name="Kianu"
-    ).update(first_name="Keanu", last_name="Reeves")
+        first_name="George",
+        last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
 
     Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(first_name="Scarlett").delete()
+    Actor.objects.filter(
+        first_name="Scarlett",
+        last_name__in=["Keegan", "Johansson"]
+    ).delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
