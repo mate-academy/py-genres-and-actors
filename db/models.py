025276a4d5db562031e5fdad94510c3f5040f205
Paweł Dudek <dudek136@gmail.com>
2026-01-02 11:49:55 +0100

    Solutionka v.2

--- db/models.py
+++ db/models.py
@@ -4,14 +4,15 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 # Genre.objects.create(name='Science Fiction')
+
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.first_name + " " + self.last_name
