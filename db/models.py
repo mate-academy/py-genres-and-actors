cc720054747d525bcb2ad2fad07c0c28bae17b86
Walery Demidko <walerydz@gmail.com>
2026-01-11 11:13:05 +0100

    Solution

--- db/models.py
+++ db/models.py
@@ -12,13 +12,5 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["first_name", "last_name"],
-                name="unique_actor_full_name"
-            )
-        ]
-
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
