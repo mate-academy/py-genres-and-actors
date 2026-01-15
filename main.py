8dea0ee44d0804810bf03167dacde7bacb5b7a6e
Farlinder <sashakost2006e@gmail.com>
2026-01-15 18:54:37 +0100

    Commit 1

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+
 
 def main() -> QuerySet:
-    pass
+    genres_list = ["Western", "Action", "Dramma"]
+    actors_dict = {"George": "Klooney",
+                       "Kianu": "Reaves",
+                       "Scarlett": "Keegan",
+                       "Will": "Smith",
+                       "Jaden": "Smith",
+                       "Johanson": "Scarlett"}
+    for genre in genres_list:
+        Genre.objects.create(name=genre)
+    for actor in actors_dict.keys():
+        if actor != "Johanson":
+            Actor.objects.create(first_name = actor,
+                                 last_name = actors_dict[actor])
+        else:
+            Actor.objects.create(first_name = actors_dict[actor],
+                                 last_name = actor)
