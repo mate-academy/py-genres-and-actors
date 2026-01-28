766b0a4a458127d820c0ff7c82af2c3fe471b00a
Bohdan Borsuk <barsuchellos@gmail.com>
2026-01-28 13:10:21 +0000

    S

--- db/models.py
+++ db/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+MAX_LENGTH = 255
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=MAX_LENGTH)
+    last_name = models.CharField(max_length=MAX_LENGTH)
